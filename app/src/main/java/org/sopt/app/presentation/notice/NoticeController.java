9ee52cea5376fb98b4104ca095a6b270224e2739
MyunDev <ygj01069@naver.com>
2022-10-16 16:46:18 +0900

    [FEAT] 공지사항 조회 기능

--- app/src/main/java/org/sopt/app/presentation/notice/NoticeController.java
+++ app/src/main/java/org/sopt/app/presentation/notice/NoticeController.java
@@ -1,9 +1,40 @@
 package org.sopt.app.presentation.notice;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.sopt.app.application.notice.NoticeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-public class NoticeController {
+public class NoticeController extends BaseController {
+
+    private final NoticeService noticeService;
+
+
+    /**
+     * notice_id로 공지 조회하기
+     * @param notice_id
+     * @return
+     */
+    @GetMapping(value = "/notice/{notice_id}")
+    @ResponseBody
+    public ResponseEntity<?> findNotice(@PathVariable(required = false) Integer notice_id) {
+        return new ResponseEntity<>(noticeService.findAllById(notice_id), getSuccessHeaders(), HttpStatus.OK);
+    }
+
+    /**
+     * part 별 공지조회
+     * title로 공지조회
+     * @param part
+     * @param title
+     * @return
+     */
+    @GetMapping(value = "/notice")
+    @ResponseBody
+    public ResponseEntity<?> findNoticeByPartandTitle(@RequestParam(value = "part" , required = false) String part,
+                                        @RequestParam(value = "title" , required = false) String title) {
+        return new ResponseEntity<>(noticeService.findNoticeByPartandTitle(part, title), getSuccessHeaders(), HttpStatus.OK);
+    }
 }
