97b50bc0003272eee2331d67174f22cf0b5e0355
tape22 <gjwjdlas@gmail.com>
2022-10-22 17:08:44 +0900

    feat: savePartsController 추가

--- app/src/main/java/org/sopt/app/presentation/alert/AlertController.java
+++ app/src/main/java/org/sopt/app/presentation/alert/AlertController.java
@@ -2,26 +2,41 @@ package org.sopt.app.presentation.alert;
 
 import lombok.AllArgsConstructor;
 import org.sopt.app.application.alert.AlertService;
-import org.sopt.app.presentation.alert.dto.PartRspDTO;
+import org.sopt.app.application.alert.command.SaveAlertCommand;
+import org.sopt.app.presentation.alert.dto.PartsDTO;
+import org.sopt.app.presentation.alert.dto.SavePartsDTO;
 import org.sopt.app.presentation.notice.BaseController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class AlertController extends BaseController {
 
     private final AlertService alertService;
+
     /**
      * 파트 목록 조회하기
-     * @return
      */
     @GetMapping(value = "/alert/part")
     @ResponseBody
-    public ResponseEntity<PartRspDTO> findPart() {
+    public ResponseEntity<PartsDTO> findPart() {
         return new ResponseEntity<>(alertService.findPart(), getSuccessHeaders(), HttpStatus.OK);
+    }
+
+    /**
+     * 파트 알림정보 저장
+     */
+
+    @PostMapping(value = "/alert/{user_id}")
+    public void saveParts(
+            @PathVariable(name = "user_id") Long userId,
+            @RequestBody SavePartsDTO partsDto
+    ) {
+        alertService.saveParts(SaveAlertCommand.builder()
+                .userId(userId)
+                .partsDto(partsDto)
+                .build());
     }
 }
