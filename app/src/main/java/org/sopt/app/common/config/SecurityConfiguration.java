85bd7b550a91f3838dea8f83d50963b49ac5e3ce
tape22 <gjwjdlas@gmail.com>
2022-11-19 19:47:23 +0900

    feat: 비밀번호 암호화 및 시큐리티 설정 추가

---
+++ app/src/main/java/org/sopt/app/common/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package org.sopt.app.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
+    }
+}
