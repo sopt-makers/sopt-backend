8de3acad2acc2ab8da3f981d97ef58117f341794
tape22 <gjwjdlas@gmail.com>
2022-12-01 19:40:03 +0900

    refactor: appspec 경로 및 shell script 수정

--- script/start.sh
+++ script/start.sh
@@ -5,4 +5,4 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar -Dspring.profiles.active=dev $REPOSITORY/build/libs/app-server-0.0.1-SNAPSHOT.jar >nohup.out 2>&1 </dev/null &
+nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/build/libs/app-server-0.0.1-SNAPSHOT.jar >nohup.out 2>&1 </dev/null &
