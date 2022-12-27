fdf44f3c2af2246096ae66a3eacb8ef73a295396
tape22 <gjwjdlas@gmail.com>
2022-12-27 18:29:20 +0900

    feat: 날짜 타입을 yyyy-MM-dd로 변경

--- src/main/java/org/sopt/app/domain/entity/BaseEntity.java
+++ src/main/java/org/sopt/app/domain/entity/BaseEntity.java
@@ -1,6 +1,6 @@
 package org.sopt.app.domain.entity;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,11 +20,13 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
+    @JsonFormat(pattern ="yyyy-MM-dd")
     private LocalDateTime createdAt;
 
 
     @LastModifiedDate
     @Column(name = "updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime updatedAt;
 
 }
