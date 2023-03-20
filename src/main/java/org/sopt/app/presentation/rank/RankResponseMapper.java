8bfbf5667bca6099e0f03c061692906929d7ecd8
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-03-20 23:19:54 +0900

    refactor: controller 에서 missionList 조회 #43

--- src/main/java/org/sopt/app/presentation/rank/RankResponseMapper.java
+++ src/main/java/org/sopt/app/presentation/rank/RankResponseMapper.java
@@ -3,9 +3,13 @@ package org.sopt.app.presentation.rank;
 import java.util.List;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 import org.sopt.app.application.rank.RankInfo;
 import org.sopt.app.application.rank.RankInfo.Main;
+import org.sopt.app.domain.entity.Mission;
+import org.sopt.app.domain.entity.User;
 
 @Mapper(
         componentModel = "spring",
@@ -16,7 +20,8 @@ public interface RankResponseMapper {
 
     List<RankResponse.Main> of(List<Main> rank);
 
-    RankResponse.Detail of(RankInfo.Detail rank);
+    @Mappings({@Mapping(source = "user.id", target = "userId")})
+    RankResponse.Detail of(User user, List<Mission> userMissions);
 
     RankResponse.Profile of(RankInfo.Profile profile);
 
