c55910e3e8f3bd8f341e9ffe46de6c4299aefe65
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-04-08 02:15:32 +0900

    feat: 메인 뷰 조회 API #51

--- src/main/java/org/sopt/app/presentation/user/UserOriginalController.java
+++ src/main/java/org/sopt/app/presentation/user/UserOriginalController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.app.application.auth.PlaygroundAuthService;
-import org.sopt.app.application.user.UserService;
 import org.sopt.app.domain.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,20 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "Authorization")
 public class UserOriginalController {
 
-    private final UserService userService;
     private final PlaygroundAuthService playgroundAuthService;
     private final UserResponseMapper userResponseMapper;
 
     @Operation(summary = "메인 뷰 조회")
     @GetMapping(value = "/main")
-    public ResponseEntity<?> getMainViewInfo(@AuthenticationPrincipal User user) {
-        // TODO: 추후 플그쪽 조회해서 제공해야 함
+    public ResponseEntity<UserResponse.Main> getMainViewInfo(@AuthenticationPrincipal User user) {
         val mainViewUser = playgroundAuthService.getPlaygroundUserForMainView(user.getPlaygroundToken());
-//        val dummyUser = UserResponse.User.builder().status("ACTIVE").name("김솝트").profileImage("")
-//                .generationList(List.of(32L, 30L, 29L)).build();
-//        val dummyOperation = UserResponse.Operation.builder().announcement("공지다!").attendanceScore(2D).build();
-//        val response = userResponseMapper.ofMainView(dummyUser, dummyOperation);
-        return ResponseEntity.status(HttpStatus.OK).body(mainViewUser);
+        val dummyOperation = UserResponse.Operation.builder().announcement("공지다!").attendanceScore(2D).build();
+        val response = userResponseMapper.ofMainView(mainViewUser, dummyOperation);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
