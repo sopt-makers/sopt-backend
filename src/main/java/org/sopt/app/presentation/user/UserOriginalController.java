2bd7e5d745d9a6a74842a922f301cecea7c6d6bc
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-04-12 17:45:31 +0900

    feat: 운영팀 출석 조회 메인 뷰 추가 #55

--- src/main/java/org/sopt/app/presentation/user/UserOriginalController.java
+++ src/main/java/org/sopt/app/presentation/user/UserOriginalController.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.app.application.auth.PlaygroundAuthService;
+import org.sopt.app.application.operation.OperationService;
 import org.sopt.app.domain.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,15 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserOriginalController {
 
     private final PlaygroundAuthService playgroundAuthService;
+    private final OperationService operationService;
     private final UserResponseMapper userResponseMapper;
 
     @Operation(summary = "메인 뷰 조회")
     @GetMapping(value = "/main")
-    public ResponseEntity<UserResponse.Main> getMainViewInfo(@AuthenticationPrincipal User user) {
+    public ResponseEntity<UserResponse.Main> getMainViewInfo(
+            @AuthenticationPrincipal User user,
+            @RequestHeader("Authorization") String accessToken
+    ) {
         val mainViewUser = playgroundAuthService.getPlaygroundUserForMainView(user.getPlaygroundToken());
-        val dummyOperation = UserResponse.Operation.builder().announcement("공지다!").attendanceScore(2D).build();
-        val response = userResponseMapper.ofMainView(mainViewUser, dummyOperation);
+        val mainViewOperation = operationService.getOperationForMainView(accessToken);
+        val response = userResponseMapper.ofMainView(mainViewUser, mainViewOperation);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-
 }
