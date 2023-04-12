6ef0dc186a0feeac777be97c4c992f0d8a2938c8
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-04-12 22:55:55 +0900

    refactor: 미션 생성하기 API #57

--- src/main/java/org/sopt/app/presentation/mission/MissionController.java
+++ src/main/java/org/sopt/app/presentation/mission/MissionController.java
@@ -14,10 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
@@ -42,13 +41,8 @@ public class MissionController {
     @Operation(summary = "미션 생성하기")
     @PostMapping("")
     public ResponseEntity<MissionResponse.Id> registerMission(
-            @RequestPart("missionContent") MissionRequest.RegisterMissionRequest registerMissionRequest,
-            @RequestPart(name = "imgUrl", required = false) List<MultipartFile> multipartFiles) {
+            @RequestBody MissionRequest.RegisterMissionRequest registerMissionRequest) {
         val mission = missionService.uploadMission(registerMissionRequest);
-        val imgPaths = s3Service.upload(multipartFiles);
-        if (imgPaths.size() > 0) {
-            missionService.editMissionWithImages(mission, imgPaths);
-        }
         val response = missionResponseMapper.of(mission.getId());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
