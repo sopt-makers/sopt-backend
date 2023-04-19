22fc186d481122ab3863c1e53b2a97cd9bec3dc3
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-04-20 00:38:27 +0900

    del: sentry logging 위한 UnknownExceptionHandler 제거

--- src/main/java/org/sopt/app/common/response/CommonControllerAdvice.java
+++ src/main/java/org/sopt/app/common/response/CommonControllerAdvice.java
@@ -1,7 +1,6 @@
 package org.sopt.app.common.response;
 
 import org.sopt.app.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +14,4 @@ public class CommonControllerAdvice {
                 baseException.getResponseMessage()), null, baseException.getStatusCode());
     }
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity onException(Exception exception) {
-        exception.printStackTrace();
-        return new ResponseEntity<>(CommonResponse.onFailure(HttpStatus.INTERNAL_SERVER_ERROR,
-                "서버 에러가 발생했습니다."), null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
