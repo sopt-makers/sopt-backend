1bcd913663b3fc71de9b09d867441463d361661d
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2023-06-16 22:12:49 +0900

    feat: notification table 생성 #75

---
+++ src/main/java/org/sopt/app/domain/entity/Notification.java
@@ -0,0 +1,44 @@
+package org.sopt.app.domain.entity;
+
+import com.vladmihalcea.hibernate.type.array.ListArrayType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.TypeDef;
+
+@Entity
+@Table(name = "notification", schema = "app_dev")
+@TypeDef(
+        name = "list-array",
+        typeClass = ListArrayType.class
+)
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(columnDefinition = "TEXT")
+    @ColumnDefault("")
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    @ColumnDefault("")
+    private String content;
+
+}
