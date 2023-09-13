a54eb2ed1234fcf3e9bf1a6a9a98a9dbdacaf1af
gunom <gu3062@naver.com>
2023-09-13 22:02:04 +0900

    refactor: 공통 로직 분리 #80

--- src/main/java/org/sopt/app/application/rank/RankService.java
+++ src/main/java/org/sopt/app/application/rank/RankService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.app.application.auth.PlaygroundAuthInfo.ActiveUserIds;
+import org.sopt.app.application.rank.RankInfo.Main;
 import org.sopt.app.common.exception.BadRequestException;
 import org.sopt.app.common.response.ErrorCode;
 import org.sopt.app.domain.entity.User;
@@ -21,16 +22,7 @@ public class RankService {
 
     public List<RankInfo.Main> findRanks() {
         val userList = userRepository.findAll();
-        val rankPoint = new AtomicInteger(1);
-        return userList.stream().sorted(
-                        Comparator.comparing(User::getPoints).reversed())
-                .map(user -> RankInfo.Main.builder()
-                        .rank(rankPoint.getAndIncrement())
-                        .nickname(user.getNickname())
-                        .point(user.getPoints())
-                        .profileMessage(user.getProfileMessage())
-                        .build())
-                .collect(Collectors.toList());
+        return this.getRanking(userList);
     }
 
     public User findRankByNickname(String nickname) {
@@ -40,16 +32,20 @@ public class RankService {
 
     public List<RankInfo.Main> findCurrentRanks(ActiveUserIds activeUserIds) {
         val userList = userRepository.findAllById(activeUserIds.getMemberIds());
+        return this.getRanking(userList);
+    }
+
+    private List<Main> getRanking(List<User> userList) {
         val rankPoint = new AtomicInteger(1);
         return userList.stream().sorted(
-                        Comparator.comparing(User::getPoints).reversed())
-                .map(user -> RankInfo.Main.builder()
-                        .rank(rankPoint.getAndIncrement())
-                        .nickname(user.getNickname())
-                        .point(user.getPoints())
-                        .profileMessage(user.getProfileMessage())
-                        .build())
-                .collect(Collectors.toList());
+                Comparator.comparing(User::getPoints).reversed())
+            .map(user -> Main.builder()
+                .rank(rankPoint.getAndIncrement())
+                .nickname(user.getNickname())
+                .point(user.getPoints())
+                .profileMessage(user.getProfileMessage())
+                .build())
+            .collect(Collectors.toList());
     }
 
 }
