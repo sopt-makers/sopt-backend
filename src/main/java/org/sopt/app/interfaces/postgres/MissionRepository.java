2386b60dfc3ff2c97f1ee199d054a8863df23f7d
gunom <gu3062@naver.com>
2023-09-18 00:02:44 +0900

    refactor: mission의 display 속성 반영

--- src/main/java/org/sopt/app/interfaces/postgres/MissionRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/MissionRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
-    @Query("SELECT m FROM Mission m WHERE m.id IN :missions ORDER BY m.level, m.title")
-    List<Mission> findMissionInOrderByLevelAndTitle(@Param("missions") List<Long> missions);
+    @Query("SELECT m FROM Mission m WHERE m.id IN :missions AND m.display = true ORDER BY m.level, m.title")
+    List<Mission> findMissionInOrderByLevelAndTitleAndDisplayTrue(@Param("missions") List<Long> missions);
 
+    List<Mission> findAllByDisplay(boolean display);
 }
