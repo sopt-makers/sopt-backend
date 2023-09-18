9a3bf64557bf34e552bdb82983d53145e2e325d6
yummygyudon <bang2brew@gmail.com>
2023-09-18 21:05:32 +0900

    FIX : 분리된 `PushToken`과 관련된 탈퇴 & 로그아웃 단계에서의 로직 변경 사항 반영
    - (현재 시점) Push Server 정상화 때까지는 Delete 요청 불가
    - 구체화된 기능 정책에 따라 유동적인 변경 가능성 존재

--- src/main/java/org/sopt/app/presentation/user/UserWithdrawController.java
+++ src/main/java/org/sopt/app/presentation/user/UserWithdrawController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.sopt.app.application.notification.PushTokenService;
 import org.sopt.app.application.stamp.StampService;
 import org.sopt.app.application.user.UserService;
 import org.sopt.app.domain.entity.User;
@@ -24,6 +26,26 @@ public class UserWithdrawController {
 
     private final UserService userService;
     private final StampService stampService;
+    private final PushTokenService pushTokenService;
+
+    private final UserResponseMapper userResponseMapper;
+
+    @Operation(summary = "로그아웃하기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "success", content = @Content),
+            @ApiResponse(responseCode = "500", description = "server error", content = @Content)
+    })
+    @DeleteMapping(value = "/logout")
+    public ResponseEntity<UserResponse.AppUser> logout(@AuthenticationPrincipal User user) {
+        // 정책 : 알림 수신 여부만 비활성화 & 기존 상세 알림 설정은 유지
+        // 강제 삭제를 위한 Request 객체 생성
+        UserRequest.EditIsOptInRequest editIsOptInRequest = new UserRequest.EditIsOptInRequest();
+        editIsOptInRequest.setIsOptIn(false);
+        userService.updateIsOptIn(user, editIsOptInRequest);
+        pushTokenService.deleteAllDeviceTokenOf(user);
+        val response = userResponseMapper.ofAppUser(user);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
     @Operation(summary = "탈퇴하기")
     @ApiResponses({
@@ -33,7 +55,9 @@ public class UserWithdrawController {
     @DeleteMapping(value = "")
     public ResponseEntity<UserResponse.AppUser> withdraw(@AuthenticationPrincipal User user) {
         // TODO: S3 이미지 삭제
+        // TODO: 알림 서버 FCM Token 삭제 요청
         stampService.deleteAllStamps(user);
+        pushTokenService.deleteAllDeviceTokenOf(user);
         userService.deleteUser(user);
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
