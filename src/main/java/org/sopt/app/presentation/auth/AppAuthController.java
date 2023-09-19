d57c6067c6a7a102536b465229c3375ece449fcf
yummygyudon <bang2brew@gmail.com>
2023-09-19 15:16:24 +0900

    FIX : PushToken 복합키 필드 변경
    - 타 서비스와도 종속적인 알림 기능이기 때문에 `app_user.user_id`가 아닌 `app_user.playground_id`로 변경했습니다.

--- src/main/java/org/sopt/app/presentation/auth/AppAuthController.java
+++ src/main/java/org/sopt/app/presentation/auth/AppAuthController.java
@@ -52,12 +52,16 @@ public class AppAuthController {
 
         val userId = userService.loginWithUserPlaygroundId(playgroundMember, codeRequest);
 
+        /**
+         * 알림 기능 자체가 타 서비스들과 종속성이 있는 관계로
+         * UserId -> Playground Member Id 로 변경되었습니다.
+         */
         // 4. 기본 알림 설정 저지
-        optionService.registerOptIn(userId.getId());
+        optionService.registerOptIn(playgroundMember.getId());
 
         // 5. Push Token 등록
         val pushToken = PushToken.builder()
-                .userId(userId.getId())
+                .playgroundId(playgroundMember.getId())
                 .token(codeRequest.getPushToken())
                 .build();
         pushTokenService.registerDeviceToken(pushToken, platform);
