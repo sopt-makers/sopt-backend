89b7230235091fc8b8e658d685d6f57843921b0f
yummygyudon <bang2brew@gmail.com>
2023-09-19 15:15:04 +0900

    DOCS : PushTokenResponse DTO 설명 추가

--- src/main/java/org/sopt/app/presentation/notification/PushTokenResponse.java
+++ src/main/java/org/sopt/app/presentation/notification/PushTokenResponse.java
@@ -1,5 +1,6 @@
 package org.sopt.app.presentation.notification;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -11,10 +12,13 @@ public class PushTokenResponse {
     @ToString
     public static class StatusResponse {
 
+        @Schema(description = "알림 서버 Response Status", example = "200")
         private Integer status;
 
+        @Schema(description = "성공 여부", example = "true")
         private Boolean success;
 
+        @Schema(description = "알림 서버 Response Message", example = "토큰 해지 성공")
         private String message;
     }
 
