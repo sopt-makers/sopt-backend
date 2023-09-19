c0932c21e092ff2c8b0d7166a6b84c672dc6a22e
gunom <gu3062@naver.com>
2023-09-19 14:49:45 +0900

    feat: response 설명 추가 #83

--- src/main/java/org/sopt/app/presentation/description/DescriptionResponse.java
+++ src/main/java/org/sopt/app/presentation/description/DescriptionResponse.java
@@ -1,5 +1,6 @@
 package org.sopt.app.presentation.description;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +13,9 @@ public class DescriptionResponse {
     @ToString
     @Builder
     public static class MainDescription {
+        @Schema(description="메인 상단 문구", example = "33기 Do sopt에 오신 것을 환영합니다!")
         private String topDescription;
+        @Schema(description="메인 하단 문구", example = "벋벋조와 어쩌구")
         private String bottomDescription;
     }
 }
