6ccdefea1754514fe2cfe00d6ed037eedfb060ba
yummygyudon <bang2brew@gmail.com>
2023-09-19 15:12:07 +0900

    FEAT : 알림 서버(External) 요청 DTO 생성
    - 추후 "**유저 ID를 null로 요청해야하는**" 비회원의 Request에 대비하기 위해 `ExternalRequest` interface 를 상속 받도록 했습니다.

--- src/main/java/org/sopt/app/presentation/notification/PushTokenRequest.java
+++ src/main/java/org/sopt/app/presentation/notification/PushTokenRequest.java
@@ -3,7 +3,6 @@ package org.sopt.app.presentation.notification;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import javax.validation.constraints.NotNull;
@@ -13,19 +12,6 @@ public class PushTokenRequest {
 
     @Getter
     @Builder
-    @ToString
-    public static class RegisterRequest {
-        @Schema(description = "유저 아이디", example = "['1']")
-        @NotNull(message = "register target user ID may not be null")
-        private List<String> userIds;
-
-        @Schema(description = "푸시 토큰", example = "asdfasdf")
-        @NotNull(message = "push token may not be null")
-        private String pushToken;
-    }
-
-    @Getter
-    @Setter
     @ToString
     public static class EditRequest {
 
@@ -38,6 +24,23 @@ public class PushTokenRequest {
     @Builder
     @ToString
     public static class DeleteRequest {
+
+        @Schema(description = "푸시 토큰", example = "asdfasdf")
+        @NotNull(message = "push token may not be null")
+        private String pushToken;
+    }
+
+    public interface ExternalRequest {}
+
+    @Getter
+    @Builder
+    @ToString
+    public static class ExternalMemberRequest implements ExternalRequest{
+        //TODO : NonMember 정책 확정되면 비회원전용 Request 객체 필요
+
+        @Schema(description = "유저 아이디", example = "['1']")
+        @NotNull(message = "target user's Playground ID List used by register/delete request for Push Server may not be null")
+        private List<String> userIds;
 
         @Schema(description = "푸시 토큰", example = "asdfasdf")
         @NotNull(message = "push token may not be null")
