4e7013a62af12a1c72a3b3b453aba634953f99f4
yummygyudon <bang2brew@gmail.com>
2023-10-01 03:42:53 +0900

    FEAT : 알림서버로부터 받아온 알림에 대해 저장하는 Service 로직
    - Notification의 Type(`all`, `individual`) 여부에 따라 분기
      - `all`일 경우, 전체 app users 대상으로 저장해야 하기 때문에 `userIds`를 사용하지 않고 UserRepository에서 모든 app users 들의 playground Id들을 조회하여 사용
      - `individual`일 경우, 특정 app users 대상으로 저장해야 하기 때문에 DTO로부터 `userIds`를 받아와 저장
    - 이미 토큰 등록하며 알림서버에 저장된 playgroundId 들은 유효하다는 것이 보장된 상태여서 조회 결과 검증은 수행하지 않았습니다

--- src/main/java/org/sopt/app/application/notification/NotificationService.java
+++ src/main/java/org/sopt/app/application/notification/NotificationService.java
@@ -1,14 +1,19 @@
 package org.sopt.app.application.notification;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.app.common.exception.BadRequestException;
 import org.sopt.app.common.response.ErrorCode;
 import org.sopt.app.domain.entity.Notification;
 import org.sopt.app.domain.entity.User;
+import org.sopt.app.domain.enums.NotificationType;
 import org.sopt.app.interfaces.postgres.NotificationRepository;
+import org.sopt.app.interfaces.postgres.UserRepository;
 import org.sopt.app.presentation.notification.NotificationRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,31 +24,48 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public List<Notification> findNotificationList(User user, Pageable pageable) {
-        val notificationList = notificationRepository.findAllByUserId(user.getId(), pageable);
-        return notificationList;
+        return notificationRepository.findAllByPlaygroundId(user.getPlaygroundId(), pageable);
     }
 
     @Transactional
-    public Notification registerNotification(
-            Long userId,
+    public void registerNotification(
             NotificationRequest.RegisterNotificationRequest registerNotificationRequest
     ) {
-        val notification = Notification.builder()
-                .userId(userId)
-                .title(registerNotificationRequest.getTitle())
-                .content(registerNotificationRequest.getContent())
-//                .type(registerNotificationRequest.getType())
-                .isRead(false)
-                .build();
-        return notificationRepository.save(notification);
+        List<Long> playgroundIds = new ArrayList<>();
+        System.out.println(registerNotificationRequest.toString());
+        if (registerNotificationRequest.getType().equals(NotificationType.ALL)) {
+            playgroundIds = userRepository.findAllPlaygroundId();
+        } else if (registerNotificationRequest.getType().equals(NotificationType.INDIVIDUAL)) {
+            playgroundIds = registerNotificationRequest.getUserIds().stream()
+                    .map(Long::parseLong)
+                    .toList();
+        }
+        registerTo(playgroundIds, registerNotificationRequest);
+    }
+    private void registerTo(List<Long> playgroundIds, NotificationRequest.RegisterNotificationRequest registerNotificationRequest) {
+        for (Long playgroundId : playgroundIds) {
+            val notification = Notification.builder()
+                    .playgroundId(playgroundId)
+                    .messageId(registerNotificationRequest.getMessageId())
+                    .title(registerNotificationRequest.getTitle())
+                    .content(registerNotificationRequest.getContent())
+                    .type(registerNotificationRequest.getType())
+                    .category(registerNotificationRequest.getCategory())
+                    .deepLink(registerNotificationRequest.getDeepLink())
+                    .webLink(registerNotificationRequest.getWebLink())
+                    .isRead(false)
+                    .build();
+            notificationRepository.save(notification);
+        }
     }
 
     @Transactional
     public void updateNotificationIsRead(User user, Long notificationId) {
-        if (notificationId == 0) {
+        if (Objects.isNull(notificationId)) {
             updateAllNotificationIsRead(user);
         } else {
             updateSingleNotificationIsRead(user, notificationId);
@@ -51,16 +73,18 @@ public class NotificationService {
     }
 
     private void updateAllNotificationIsRead(User user) {
-        val notificationList = notificationRepository.findAllByUserId(user.getId());
-        val readNotificationList = notificationList.stream().map(notification -> {
-            notification.updateIsRead();
-            return notification;
-        }).collect(Collectors.toList());
+        val notificationList = notificationRepository.findAllByPlaygroundId(user.getId());
+        val readNotificationList = notificationList.stream()
+                .map(notification -> {
+                        notification.updateIsRead();
+                        return notification;
+                    }
+                ).collect(Collectors.toList());
         notificationRepository.saveAll(readNotificationList);
     }
 
     private void updateSingleNotificationIsRead(User user, Long notificationId) {
-        val notification = notificationRepository.findByIdAndUserId(notificationId, user.getId())
+        val notification = notificationRepository.findByIdAndPlaygroundId(notificationId, user.getPlaygroundId())
                 .orElseThrow(() -> new BadRequestException(ErrorCode.NOTIFICATION_NOT_FOUND.getMessage()));
         notification.updateIsRead();
         notificationRepository.save(notification);
@@ -68,10 +92,10 @@ public class NotificationService {
 
     @Transactional(readOnly = true)
     public Boolean getNotificationMainViewStatus(User user) {
-        val notificationList = notificationRepository.findAllByUserId(user.getId());
+        val notificationList = notificationRepository.findAllByPlaygroundId(user.getPlaygroundId());
         val unreadNotificationList = notificationList.stream()
                 .filter(notification -> !notification.getIsRead())
-                .collect(Collectors.toList());
+                .toList();
         return unreadNotificationList.size() > 0;
     }
 }
--- src/main/java/org/sopt/app/interfaces/postgres/UserRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import org.sopt.app.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long userId);
@@ -11,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByPlaygroundId(Long playgroundId);
 
     List<User> findAllByPlaygroundIdIn(List<Long> playgroundIds);
+
+    @Query("SELECT DISTINCT u.playgroundId FROM User u")
+    List<Long> findAllPlaygroundId();
 }
