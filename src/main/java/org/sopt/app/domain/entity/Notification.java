f94e973b64f1fe39816fa558d6f2bda28615e98d
yummygyudon <bang2brew@gmail.com>
2023-10-04 20:39:36 +0900

    FIX : Notification BaseEntity 상속 구조로 전환

--- src/main/java/org/sopt/app/domain/entity/Notification.java
+++ src/main/java/org/sopt/app/domain/entity/Notification.java
@@ -1,8 +1,5 @@
 package org.sopt.app.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDateTime;
 import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
@@ -12,10 +9,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.sopt.app.domain.enums.NotificationCategory;
 import org.sopt.app.domain.enums.NotificationType;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "notifications", schema = "app_dev")
@@ -23,8 +16,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Notification{
+public class Notification extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,17 +49,6 @@ public class Notification{
     @Column(nullable = false, name = "is_read")
     @ColumnDefault("false")
     private Boolean isRead;
-
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    @DateTimeFormat()
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
-    private LocalDateTime updatedAt;
 
 
     public void updateIsRead() {
