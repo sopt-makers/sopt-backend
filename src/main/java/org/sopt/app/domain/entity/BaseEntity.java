243816e9edeb1e7e5dd43d73315ddd101b4f0baa
yummygyudon <bang2brew@gmail.com>
2023-10-04 20:42:03 +0900

    FIX : Time Format 수정 및 Time Zone 지정
    - 직렬화 대비 `@DateTimeFormat` 설정 추가
      - iso : ISO.DATE_TIME (우리는 시간까지 사용하기때문에)
      - pattern : `yyyy-MM-dd HH:mm:ss`
    - 기존 `@JsonFormat` pattern 수정 및 timezone 지정
      - timezone : 'Asia/Seoul'
      - pattern : `yyyy-MM-dd` → `yyyy-MM-dd HH:mm:ss`

--- src/main/java/org/sopt/app/domain/entity/BaseEntity.java
+++ src/main/java/org/sopt/app/domain/entity/BaseEntity.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -20,13 +21,15 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    @JsonFormat(pattern ="yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt;
 
 }
