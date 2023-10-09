3f1078d2a2f7d5f48f6339bbfabf8b23103aa751
gunom <gu3062@naver.com>
2023-10-09 22:35:02 +0900

    refactor: DescriptionFacade 추가 및 적용

--- src/main/java/org/sopt/app/presentation/description/DescriptionController.java
+++ src/main/java/org/sopt/app/presentation/description/DescriptionController.java
@@ -7,15 +7,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
-import org.sopt.app.application.auth.PlaygroundAuthService;
+import org.sopt.app.facade.DescriptionFacade;
+import org.sopt.app.domain.entity.User;
 import org.springframework.http.HttpStatus;
-import org.sopt.app.application.description.DescriptionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.sopt.app.domain.entity.User;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +22,7 @@ import org.sopt.app.domain.entity.User;
 @SecurityRequirement(name = "Authorization")
 public class DescriptionController {
 
-    private final DescriptionService descriptionService;
-    private final PlaygroundAuthService playgroundAuthService;
+    private final DescriptionFacade descriptionFacade;
 
     @Operation(summary = "메인 문구 조회")
     @ApiResponses({
@@ -36,8 +34,7 @@ public class DescriptionController {
     public ResponseEntity<DescriptionResponse.MainDescription> getMainDescription(
         @AuthenticationPrincipal User user
     ) {
-        val userActiveInfo = playgroundAuthService.getPlaygroundUserActiveInfo(user.getPlaygroundToken());
-        val response = descriptionService.getMainDescription(userActiveInfo.getStatus());
+        val response = descriptionFacade.getMainDescriptionForUser(user);
         return ResponseEntity.status(HttpStatus.OK).body(
             DescriptionResponse.MainDescription.builder()
                 .topDescription(response.getTopDescription())
