5eb3f6cfa9780a004468d86a51093d5fb1e5417b
yummygyudon <bang2brew@gmail.com>
2023-10-19 04:44:21 +0900

    FIX : Spring Cloud OpenFeign 해제 및 GitHub OpenFeign 설정 추가

---
+++ src/main/java/org/sopt/app/common/config/ClientConfig.java
@@ -0,0 +1,44 @@
+package org.sopt.app.common.config;
+
+
+import feign.Feign;
+import feign.Retryer;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.okhttp.OkHttpClient;
+import feign.slf4j.Slf4jLogger;
+import org.sopt.app.application.auth.client.PlaygroundClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfig {
+    @Value("${makers.playground.server}")
+    private String playgroundUrl;
+
+    @Bean
+    public PlaygroundClient playgroundClient() {
+        return Feign.builder()
+                .retryer(retryer())
+                .client(new OkHttpClient())
+                .encoder(encoder())
+                .decoder(decoder())
+                .logger(new Slf4jLogger(PlaygroundClient.class))
+                .target(PlaygroundClient.class, playgroundUrl);
+    }
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default();
+    }
+    @Bean
+    public Encoder encoder() {
+        return new JacksonEncoder();
+    }
+    @Bean
+    public Decoder decoder() {
+        return new JacksonDecoder();
+    }
+}
