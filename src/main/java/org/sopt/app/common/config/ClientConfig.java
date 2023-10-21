4022358348cfeae4b8c202348cd6374dc0da557d
yummygyudon <bang2brew@gmail.com>
2023-10-22 03:41:03 +0900

    CHORE : FeignClient Config

--- src/main/java/org/sopt/app/common/config/ClientConfig.java
+++ src/main/java/org/sopt/app/common/config/ClientConfig.java
@@ -1,8 +1,7 @@
 package org.sopt.app.common.config;
 
 
-import feign.Feign;
-import feign.Retryer;
+import feign.*;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.jackson.JacksonDecoder;
@@ -10,6 +9,7 @@ import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 import org.sopt.app.application.auth.client.PlaygroundClient;
+import org.sopt.app.application.operation.client.OperationClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,21 +17,40 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ClientConfig {
     @Value("${makers.playground.server}")
-    private String playgroundUrl;
+    private String playgroundEndPoint;
+
+    @Value("${makers.operation.server}")
+    private String operationEndPoint;
+
+    @Value("${makers.push.server}")
+    private String pushEndPoint;
 
     @Bean
     public PlaygroundClient playgroundClient() {
         return Feign.builder()
                 .retryer(retryer())
-                .client(new OkHttpClient())
+                .client(okHttpClient())
                 .encoder(encoder())
                 .decoder(decoder())
                 .logger(new Slf4jLogger(PlaygroundClient.class))
-                .target(PlaygroundClient.class, playgroundUrl);
+                .logLevel(feignLoggerLevel())
+                .target(PlaygroundClient.class, playgroundEndPoint);
+    }
+
+    @Bean
+    public OperationClient operationClient() {
+        return Feign.builder()
+                .retryer(retryer())
+                .client(okHttpClient())
+                .encoder(encoder())
+                .decoder(decoder())
+                .logger(new Slf4jLogger(OperationClient.class))
+                .logLevel(feignLoggerLevel())
+                .target(OperationClient.class, operationEndPoint);
     }
     @Bean
     public Retryer retryer() {
-        return new Retryer.Default();
+        return Retryer.NEVER_RETRY;
     }
     @Bean
     public Encoder encoder() {
@@ -41,4 +60,17 @@ public class ClientConfig {
     public Decoder decoder() {
         return new JacksonDecoder();
     }
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient();
+    }
+//    @Bean
+//    public Contract feignContract() {
+//        return new Contract.Default();
+//    }
+
 }
