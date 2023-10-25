35ab5a57bee4378dd1b55cad5a3ab4b61828a815
yummygyudon <bang2brew@gmail.com>
2023-10-26 05:28:35 +0900

    FIX : 실행 경우에 따른 명령어 분기처리

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,25 @@
-REPOSITORY=/home/ubuntu/app
-cd $REPOSITORY
+DEFAULT_PATH=/home/ubuntu
+# shellcheck disable=SC2164
+cd $DEFAULT_PATH
 
+if [ ! -d app ]; then
+  # Dev 서버 배포의 경우
+  APPLICATION_PACKAGE=sopt-backend
+  PROFILE=local
+elif [ ! -d sopt-backend ]; then
+  # Prod 서버 배포의 경우
+  APPLICATION_PACKAGE=app
+  PROFILE=prod
+fi
+echo "> Target Profile : $PROFILE"
+# shellcheck disable=SC2164
+cd $APPLICATION_PACKAGE
+REPOSITORY=/home/ubuntu/$APPLICATION_PACKAGE리
+
+# shellcheck disable=SC2010
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+
 echo "> $JAR_PATH 배포" #3
-nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/build/libs/app-server-0.0.1-SNAPSHOT.jar >nohup.out 2>&1 </dev/null &
+nohup java -jar -Dspring.profiles.active=$PROFILE $REPOSITORY/build/libs/app-server-0.0.1-SNAPSHOT.jar >nohup.out 2>&1 </dev/null &
