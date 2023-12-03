d5db0a11f0c9126732c664fac300a0fdb1212052
gunom <gu3062@naver.com>
2023-12-03 21:34:36 +0900

    fix: UserService findRandomFriendsOfFriends 메소드 추가

--- src/main/java/org/sopt/app/application/user/UserService.java
+++ src/main/java/org/sopt/app/application/user/UserService.java
@@ -1,5 +1,6 @@
 package org.sopt.app.application.user;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -21,42 +22,44 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Transactional
     public UserInfo.Id loginWithUserPlaygroundId(
-            PlaygroundAuthInfo.PlaygroundMain playgroundMemberResponse
+        PlaygroundAuthInfo.PlaygroundMain playgroundMemberResponse
     ) {
-        val registeredUser = userRepository.findUserByPlaygroundId(playgroundMemberResponse.getId());
+        val registeredUser = userRepository.findUserByPlaygroundId(
+            playgroundMemberResponse.getId());
 
         // 기존에 로그인/가입한 이력이 있으면
         if (registeredUser.isPresent()) {
             registeredUser.get().updatePlaygroundUserInfo(
-                    playgroundMemberResponse.getName(),
-                    playgroundMemberResponse.getAccessToken()
+                playgroundMemberResponse.getName(),
+                playgroundMemberResponse.getAccessToken()
             );
             userRepository.save(registeredUser.get());
 
             return UserInfo.Id.builder()
-                    .id(registeredUser.get().getId()).build();
+                .id(registeredUser.get().getId()).build();
         } else {
             val newUser = this.registerNewUser(
-                    playgroundMemberResponse.getName(),
-                    playgroundMemberResponse.getId(),
-                    playgroundMemberResponse.getAccessToken()
+                playgroundMemberResponse.getName(),
+                playgroundMemberResponse.getId(),
+                playgroundMemberResponse.getAccessToken()
             );
             userRepository.save(newUser);
 
             return UserInfo.Id.builder()
-                    .id(newUser.getId()).build();
+                .id(newUser.getId()).build();
         }
     }
 
     private User registerNewUser(String username, Long playgroundId, String playgroundToken) {
         return User.builder()
-                .username(username)
-                .playgroundId(playgroundId)
-                .playgroundToken(playgroundToken)
-                .build();
+            .username(username)
+            .playgroundId(playgroundId)
+            .playgroundToken(playgroundToken)
+            .build();
     }
 
     @Transactional
@@ -67,7 +70,8 @@ public class UserService {
     @Transactional(readOnly = true)
     public AppAuthRequest.AccessTokenRequest getPlaygroundToken(UserInfo.Id userId) {
         val user = userRepository.findUserById(userId.getId())
-                .orElseThrow(() -> new UnauthorizedException(ErrorCode.INVALID_REFRESH_TOKEN.getMessage()));
+            .orElseThrow(
+                () -> new UnauthorizedException(ErrorCode.INVALID_REFRESH_TOKEN.getMessage()));
         val token = new AppAuthRequest.AccessTokenRequest();
         token.setAccessToken(user.getPlaygroundToken());
         return token;
@@ -76,13 +80,14 @@ public class UserService {
     @Transactional
     public void updatePlaygroundToken(UserInfo.Id userId, String playgroundToken) {
         val user = userRepository.findUserById(userId.getId())
-                .orElseThrow(() -> new UnauthorizedException(ErrorCode.INVALID_REFRESH_TOKEN.getMessage()));
+            .orElseThrow(
+                () -> new UnauthorizedException(ErrorCode.INVALID_REFRESH_TOKEN.getMessage()));
         val newUser = User.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .playgroundId(user.getPlaygroundId())
-                .playgroundToken(playgroundToken)
-                .build();
+            .id(user.getId())
+            .username(user.getUsername())
+            .playgroundId(user.getPlaygroundId())
+            .playgroundToken(playgroundToken)
+            .build();
         userRepository.save(newUser);
     }
 
@@ -118,4 +123,13 @@ public class UserService {
         }).toList();
     }
     public List<UserInfo.UserProfile> findRandomFriendsOfFriends(Long userId, Long friendIds, int limitNum) {
+        val users = userRepository.findRandomFriendsOfFriends(userId, friendIds, limitNum);
+        return users.stream().map(
+            u -> UserProfile.builder()
+                .userId(u.getId())
+                .name(u.getUsername())
+                .playgroundId(u.getPlaygroundId())
+                .build()
+        ).toList();
+    }
 }
