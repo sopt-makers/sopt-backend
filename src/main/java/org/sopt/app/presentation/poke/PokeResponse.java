7aea44eb85410e58ee7e87ef4246fbac1a1b328d
gunom <gu3062@naver.com>
2023-12-03 14:49:21 +0900

    feat: 콕 찌르기 신규 유저 조회 API

--- src/main/java/org/sopt/app/presentation/poke/PokeResponse.java
+++ src/main/java/org/sopt/app/presentation/poke/PokeResponse.java
@@ -1,6 +1,8 @@
 package org.sopt.app.presentation.poke;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import org.sopt.app.application.poke.PokeInfo;
@@ -8,38 +10,47 @@ import org.sopt.app.application.poke.PokeInfo;
 import java.util.List;
 
 public class PokeResponse {
+//
+//    @Getter
+//    @ToString
+//    public record PokeMessages(
+//            @Schema(description = "찌르기 메시지 리스트", example = "['메시지A', '메시지B', '메시지C', '메시지D', '메시지E']")
+//            List<String> messages
+//    ) {
+//    }
+//
+//    @Getter
+//    @ToString
+//    public record SimplePokeProfile(
+//            @Schema(description = "유저 ID", example = "1")
+//            Long userId,
+//            @Schema(description = "프로필 사진 URL", example = ".....")
+//            String profileImage,
+//            @Schema(description = "유저 이름", example = "다혜다해")
+//            String name,
+//            @Schema(description = "메시지", example = "메시지A")
+//            String message,
+//            @Schema(description = "SOPT 최신 활동 기수 정보", example = "{'generation': 29, 'part': '안드로이드'}")
+//            PokeInfo.Activity activities,
+//            @Schema(description = "현재까지 찌른 횟수", example = "3")
+//            Long pickNum,
+//            @Schema(description = "함께 친구 관계인 친구들의 이름", example = "['제갈송현', '왕건모', '진동규', '차승호']")
+//            List<String> mutual,
+//            @Schema(description = "이전에 찌른 이력이 있는지에 대한 여부", example = "false")
+//            Boolean isFirstMeet,
+//            @Schema(description = "이미 오늘 찔렀는지에 대한 여부", example = "true")
+//            Boolean isAlreadyPoke
+//    ) {
+//    }
 
     @Getter
-    @ToString
-    public record PokeMessages(
-            @Schema(description = "찌르기 메시지 리스트", example = "['메시지A', '메시지B', '메시지C', '메시지D', '메시지E']")
-            List<String> messages
-    ) {
-    }
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class IsNew {
+        @Schema(description = "새로운 유저 여부", example = "true")
+        private Boolean isNew;
 
-    @Getter
-    @ToString
-    public record SimplePokeProfile(
-            @Schema(description = "유저 ID", example = "1")
-            Long userId,
-            @Schema(description = "프로필 사진 URL", example = ".....")
-            String profileImage,
-            @Schema(description = "유저 이름", example = "다혜다해")
-            String name,
-            @Schema(description = "메시지", example = "메시지A")
-            String message,
-            @Schema(description = "SOPT 최신 활동 기수 정보", example = "{'generation': 29, 'part': '안드로이드'}")
-            PokeInfo.Activity activities,
-            @Schema(description = "현재까지 찌른 횟수", example = "3")
-            Long pickNum,
-            @Schema(description = "함께 친구 관계인 친구들의 이름", example = "['제갈송현', '왕건모', '진동규', '차승호']")
-            List<String> mutual,
-            @Schema(description = "이전에 찌른 이력이 있는지에 대한 여부", example = "false")
-            Boolean isFirstMeet,
-            @Schema(description = "이미 오늘 찔렀는지에 대한 여부", example = "true")
-            Boolean isAlreadyPoke
-    ) {
+        public static IsNew of(Boolean isNew) {
+            return new IsNew(isNew);
+        }
     }
-
 }
-
