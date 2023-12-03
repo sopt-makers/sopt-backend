b9ea0e1f31bd82c5019236b535ff09c8c62d0e0e
gunom <gu3062@naver.com>
2023-12-03 21:30:42 +0900

    feat: Friend QueryDSL repository 추가

--- src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
@@ -3,6 +3,6 @@ package org.sopt.app.interfaces.postgres;
 import org.sopt.app.domain.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FriendRepository extends JpaRepository<Friend, Integer> {
+public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCustomRepository {
 
 }
