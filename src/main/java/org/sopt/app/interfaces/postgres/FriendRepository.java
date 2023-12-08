50723ebd837a49a0a70d261c225e0c846351cdf7
yummygyudon <bang2brew@gmail.com>
2023-12-08 22:10:48 +0900

    CHORE : Rebase 이후 마지막 정리

--- src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCustomRepository  {
     Optional<Friend> findByUserIdAndFriendUserId(Long userId, Long friendId);
-
-    List<Friend> findAllByUserId(Long userId);
 
     // SELECT * FROM app_dev.friend
     //WHERE friend.user_id = 19
