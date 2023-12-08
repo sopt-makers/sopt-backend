5c63c996d680a5f78d53d5d2dacdd18021a43bc6
yummygyudon <bang2brew@gmail.com>
2023-12-08 21:19:48 +0900

    CHORE : Rebasing sprint2/poke

--- src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/FriendRepository.java
@@ -3,12 +3,13 @@ package org.sopt.app.interfaces.postgres;
 import java.util.List;
 import org.sopt.app.domain.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
 
-public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCustomRepository {
-    Optional<Friend> findByUserIdAndAndFriendUserId(Long userId, Long friendId);
+public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCustomRepository  {
+    Optional<Friend> findByUserIdAndFriendUserId(Long userId, Long friendId);
 
     List<Friend> findAllByUserId(Long userId);
 
@@ -18,4 +19,6 @@ public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCus
     //AND friend_user_id not in (30)
     @Query(value = "SELECT f.friendUserId FROM Friend as f WHERE f.userId = :userId AND f.friendUserId not in :pokedFriendIds AND f.friendUserId not in :pokeFriendIds")
     List<Long> findAllByUserIdAndFriendUserIdNotInAndFriendUserIdNotIn(Long userId, List<Long> pokedFriendIds, List<Long> pokeFriendIds);
+    @Query("SELECT f.friendUserId FROM Friend f WHERE f.userId = :userId")
+    List<Long> findAllOfFriendIdsByUserId(@Param("userId") Long userId);
 }
