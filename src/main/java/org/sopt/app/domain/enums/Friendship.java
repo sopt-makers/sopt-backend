922d47b809974b65599aa25e15700940b695ec03
yummygyudon <bang2brew@gmail.com>
2023-12-10 03:45:46 +0900

    FIX : 친구관계 기준 하한값, 상한값 Enum에서 관리할 수 있도록 수정

--- src/main/java/org/sopt/app/domain/enums/Friendship.java
+++ src/main/java/org/sopt/app/domain/enums/Friendship.java
@@ -5,9 +5,11 @@ import lombok.*;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Friendship {
-    NON_FRIEND(""), NEW_FRIEND("친한친구"), BEST_FRIEND("단짝친구"), SOULMATE("천생연분")
+    NON_FRIEND("", 0,1), NEW_FRIEND("친한친구",2, 4), BEST_FRIEND("단짝친구",5,10), SOULMATE("천생연분",11,99)
     ;
 
     private final String value;
+    private final Integer lowerLimit;
+    private final Integer upperLimit;
 
 }
