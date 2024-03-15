4fdd000883571be1b20d18464623320e50c561ad
주어진사랑(eojinjoo) <eojinjoo@gmarket.com>
2024-03-15 23:59:05 +0900

    feat: DescriptionTest 전체 수정, ControllerTest 삭제

--- src/test/java/org/sopt/app/application/DescriptionServiceTest.java
+++ src/test/java/org/sopt/app/application/DescriptionServiceTest.java
@@ -1,33 +1,61 @@
 package org.sopt.app.application;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.sopt.app.application.description.DescriptionInfo;
 import org.sopt.app.application.description.DescriptionService;
+import org.sopt.app.domain.entity.MainDescription;
 import org.sopt.app.domain.enums.UserStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.sopt.app.interfaces.postgres.MainDescriptionRepository;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DescriptionServiceTest {
 
-    @Autowired
-    private DescriptionService descriptionService;
+    private static MainDescriptionRepository mainDescriptionRepository = Mockito.mock(MainDescriptionRepository.class);
 
-    @Test
-    @DisplayName("200 - getMainDescriptionSuccessActive")
-    void getMainDescriptionSuccessActive() {
-        DescriptionInfo.MainDescription result = descriptionService.getMainDescription(UserStatus.ACTIVE);
-        Assertions.assertEquals("33기 DO SOPT, 지금 출발합니다!", result.getTopDescription());
-        Assertions.assertEquals("벋벋조와 함께하는 도장찍기!", result.getBottomDescription());
+    private DescriptionService descriptionService = new DescriptionService(
+            mainDescriptionRepository
+    );
+
+    @BeforeAll
+    private static void beforeTest() {
+        Mockito.when(mainDescriptionRepository.findAll()).thenReturn(List.of(
+                MainDescription.builder()
+                        .id(1L)
+                        .activeTopDescription("activeTop")
+                        .activeBottomDescription("activeBottom")
+                        .inactiveTopDescription("inactiveTop")
+                        .inactiveBottomDescription("inactiveBottom")
+                        .build()
+        ));
     }
 
     @Test
-    @DisplayName("200 - getMainDescriptionSuccessInActive")
+    @DisplayName("SUCCESS - getMainDescriptionSuccessActive")
+    void getMainDescriptionSuccessActive() {
+        DescriptionInfo.MainDescription result = descriptionService.getMainDescription(UserStatus.ACTIVE);
+        Assertions.assertEquals("activeTop", result.getTopDescription());
+        Assertions.assertEquals("activeBottom", result.getBottomDescription());
+    }
+
+    @Test
+    @DisplayName("SUCCESS - getMainDescriptionSuccessInactive")
     void getMainDescriptionSuccessInactive() {
         DescriptionInfo.MainDescription result = descriptionService.getMainDescription(UserStatus.INACTIVE);
-        Assertions.assertEquals("더욱 편리해진 SOPT를 이용해보세요!", result.getTopDescription());
-        Assertions.assertEquals("SOPT를 알차게 즐기고 싶다면?", result.getBottomDescription());
+        Assertions.assertEquals("inactiveTop", result.getTopDescription());
+        Assertions.assertEquals("inactiveBottom", result.getBottomDescription());
+    }
+
+    @Test
+    @DisplayName("SUCCESS - getMainDescriptionSuccessUnauthenticated")
+    void getMainDescriptionSuccessUnauthenticated() {
+        DescriptionInfo.MainDescription result = descriptionService.getMainDescription(UserStatus.UNAUTHENTICATED);
+        Assertions.assertEquals("inactiveTop", result.getTopDescription());
+        Assertions.assertEquals("inactiveBottom", result.getBottomDescription());
     }
 }
--- src/test/java/org/sopt/app/interfaces/postgres/MainDescriptionRepositoryTest.java
+++ src/test/java/org/sopt/app/interfaces/postgres/MainDescriptionRepositoryTest.java
@@ -1,20 +1,19 @@
 package org.sopt.app.interfaces.postgres;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class MainDescriptionTest {
+public class MainDescriptionRepositoryTest {
 
     @Autowired
     private MainDescriptionRepository mainDescriptionRepository;
 
     @Test
-    @DisplayName("200 - findAllSuccessWithSizeOne")
-    void findAllSuccessWithSizeOne() {
-        Assertions.assertEquals(1, mainDescriptionRepository.findAll().size());
+    @DisplayName("SUCCESS - findAll")
+    void findAll() {
+        System.out.println(mainDescriptionRepository.findAll());
     }
 }
