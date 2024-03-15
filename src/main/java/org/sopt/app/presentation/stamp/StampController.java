de9f3118701d37d1bf3b6d7c4cc709ec28dd3d47
kseysh <kseysh123@naver.com>
2024-03-15 18:00:09 +0900

    chore: 코드 포맷 맞춰 수정

--- src/main/java/org/sopt/app/presentation/stamp/StampController.java
+++ src/main/java/org/sopt/app/presentation/stamp/StampController.java
@@ -49,7 +49,8 @@ public class StampController {
     public ResponseEntity<StampResponse.StampMain> findStampByMissionAndUserId(
             @Valid @ModelAttribute StampRequest.FindStampRequest findStampRequest
     ) {
-        val result = soptampFacade.getStampInfo(findStampRequest.getMissionId(), findStampRequest.getNickname());
+        val result = soptampFacade.getStampInfo(findStampRequest.getMissionId(),
+                findStampRequest.getNickname());
         val response = stampResponseMapper.of(result);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
@@ -79,7 +80,8 @@ public class StampController {
             @RequestPart("stampContent") StampRequest.RegisterStampRequest registerStampRequest,
             @RequestPart(name = "imgUrl", required = false) List<MultipartFile> multipartFiles
     ) {
-        val result = soptampFacade.uploadStampDeprecated(user.getId(), missionId, registerStampRequest, multipartFiles);
+        val result = soptampFacade.uploadStampDeprecated(user.getId(), missionId,
+                registerStampRequest, multipartFiles);
         val response = stampResponseMapper.of(result);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
@@ -92,7 +94,8 @@ public class StampController {
             @RequestPart(value = "stampContent", required = false) StampRequest.EditStampRequest editStampRequest,
             @RequestPart(name = "imgUrl", required = false) List<MultipartFile> multipartFiles
     ) {
-        val stamp = soptampFacade.editStamp(editStampRequest, user.getId(), missionId, multipartFiles);
+        val stamp = soptampFacade.editStamp(editStampRequest, user.getId(), missionId,
+                multipartFiles);
         val response = stampResponseMapper.of(stamp.getId());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
@@ -152,7 +155,8 @@ public class StampController {
             @ApiResponse(responseCode = "500", description = "server error", content = @Content)
     })
     @DeleteMapping("/all")
-    public ResponseEntity<StampResponse.StampMain> deleteStampByUserId(@AuthenticationPrincipal User user) {
+    public ResponseEntity<StampResponse.StampMain> deleteStampByUserId(
+            @AuthenticationPrincipal User user) {
         soptampFacade.deleteStampAll(user.getId());
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
