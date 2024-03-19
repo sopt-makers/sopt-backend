8bab8f5cd9b0497ba3721c8db049bcb6f4126519
kseysh <kseysh123@naver.com>
2024-03-20 00:23:41 +0900

    feat: application/stamp/findStamp, uploadStamp관련 테스트 추가 #217

--- src/test/java/org/sopt/app/application/StampServiceTest.java
+++ src/test/java/org/sopt/app/application/StampServiceTest.java
@@ -1,10 +1,14 @@
 package org.sopt.app.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +21,7 @@ import org.sopt.app.application.stamp.StampService;
 import org.sopt.app.common.exception.BadRequestException;
 import org.sopt.app.domain.entity.Stamp;
 import org.sopt.app.interfaces.postgres.StampRepository;
+import org.sopt.app.presentation.stamp.StampRequest.RegisterStampRequest;
 
 @ExtendWith(MockitoExtension.class)
 class StampServiceTest {
@@ -28,12 +33,14 @@ class StampServiceTest {
     private StampService stampService;
 
     @Test
-    @DisplayName("200 - findStamp Success")
-    void findStampSuccess() {
+    @DisplayName("SUCCESS_UserId와 MissionId로 스탬프를 찾으면 StampInfo.Stamp DTO 반환")
+    void SUCCESS_findStamp() {
         //given
-        final Long stampUserId = 1L;
-        final Long stampMissionId = 1L;
-        Stamp stamp = Stamp.builder().id(any(Long.class)).userId(stampUserId).missionId(stampMissionId).build();
+        val stampUserId = anyLong();
+        val stampMissionId = anyLong();
+        val stampId = 1L;
+        val stamp = Stamp.builder().id(stampId).userId(stampUserId).missionId(stampMissionId).build();
+
         //when
         Mockito.when(stampRepository.findByUserIdAndMissionId(stampUserId, stampMissionId))
                 .thenReturn(Optional.of(stamp));
@@ -44,48 +51,46 @@ class StampServiceTest {
                 .createdAt(stamp.getCreatedAt())
                 .updatedAt(stamp.getUpdatedAt())
                 .build();
-        StampInfo.Stamp actual = stampService.findStamp(stampUserId, stampMissionId);
+        StampInfo.Stamp result = stampService.findStamp(stampUserId, stampMissionId);
+
         //then
-        assertThat(actual).isInstanceOf(StampInfo.Stamp.class);
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+        Assertions.assertInstanceOf(StampInfo.Stamp.class, result);
     }
 
     @Test
-    @DisplayName("400 - 스탬프를 찾지 못하면 BadRequestException을 던진다.")
-    void findStampBadRequest() {
+    @DisplayName("FAIL_스탬프를 찾지 못하면 BadRequestException 발생")
+    void FAIL_findStampBadRequest() {
+        //given
+        val anyUserId = anyLong();
+        val anyMissionId = anyLong();
+
         //when
-        Mockito.when(stampRepository.findByUserIdAndMissionId(any(Long.class), any(Long.class)))
+        Mockito.when(stampRepository.findByUserIdAndMissionId(anyUserId, anyMissionId))
                 .thenReturn(Optional.empty());
+
         //then
-        assertThrows(BadRequestException.class, () -> {
-            stampService.findStamp(any(Long.class), any(Long.class));
+        Assertions.assertThrows(BadRequestException.class, () -> {
+            stampService.findStamp(anyUserId, anyMissionId);
         });
     }
 
-/* TODO: Implement the following tests
-
     @Test
-    void uploadStampDeprecated() {
+    @DisplayName("SUCCESS_스탬프가 request에서 보낸 내용의 이미지와 내용으로 변경되었는지 확인")
+    void SUCCESS_uploadStampDeprecated() {
         // given
-
-        final Long missionId = 1L;
-
-        List<String> imgPaths = new ArrayList<>("requestImage1", "requestImage2");
-        final Long requestUserId = 1L;
-
-        final String requestContents = "requestContents";
-        final String requestImage = "requestImage";
-        final Long requestMissionId = 1L;
+        val requestUserId = 1L;
+        val requestMissionId = 1L;
+        val requestContents = "requestContents";
+        List<String> imgPaths = List.of("requestImage1", "requestImage2");
         RegisterStampRequest stampRequest = new RegisterStampRequest();
         stampRequest.setContents(requestContents);
-        stampRequest.setImage(requestImage);
-        stampRequest.setMissionId(requestMissionId);
+        val stamp = this.convertStampImgDeprecated(stampRequest, imgPaths, requestUserId, requestMissionId);
 
         //when
-        val imgList = new ArrayList<>(imgPaths);
-        val stamp = this.convertStampImgDeprecated(stampRequest, imgList, requestUserId, requestMissionId);
+        Mockito.when(stampRepository.save(stamp)).thenReturn(stamp);
         val newStamp = stampRepository.save(stamp);
-        return StampInfo.Stamp.builder()
+        StampInfo.Stamp expected = StampInfo.Stamp.builder()
                 .id(newStamp.getId())
                 .contents(newStamp.getContents())
                 .images(newStamp.getImages())
@@ -94,11 +99,28 @@ class StampServiceTest {
                 .build();
 
         //then
-
-
+        Assertions.assertEquals(expected.getId(), stamp.getId());
+        Assertions.assertEquals(expected.getImages(), imgPaths);
+        Assertions.assertEquals(expected.getContents(), requestContents);
     }
+
+    private Stamp convertStampImgDeprecated(
+            RegisterStampRequest stampRequest,
+            List<String> imgList,
+            Long userId,
+            Long missionId) {
+        return Stamp.builder()
+                .contents(stampRequest.getContents())
+                .createdAt(LocalDateTime.now())
+                .images(imgList)
+                .missionId(missionId)
+                .userId(userId)
+                .build();
+    }
+
+    /* TODO: Implement the following tests
     @Test
-    void uploadStamp() {
+    void SUCCESS_uploadStamp() {
     }
 
     @Test
