3c1fa240b5ce6709bbf056232eb60b3e1652259d
kseysh <kseysh123@naver.com>
2024-03-23 14:40:25 +0900

    refactor: 중복되는 save함수 beforeEach로 처리 (#217)

--- src/test/java/org/sopt/app/interfaces/postgres/StampRepositoryTest.java
+++ src/test/java/org/sopt/app/interfaces/postgres/StampRepositoryTest.java
@@ -3,6 +3,7 @@ package org.sopt.app.interfaces.postgres;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sopt.app.common.config.QuerydslConfiguration;
@@ -26,30 +27,33 @@ class StampRepositoryTest {
     @Autowired
     private StampRepository stampRepository;
 
+    private final User newUser = new User();
+
+    @BeforeEach
+    public void beforeTest() {
+        userRepository.save(newUser);
+    }
+
     @Test
     @DisplayName("SUCCESS_유저 아이디를 이용하여 유저의 스탬프 모두 조회")
     void SUCCESS_findAllByUserId() {
-        //given
-        final User user = userRepository.save(new User());
-
         //when
-        Stamp stamp1 = stampRepository.save(Stamp.builder().userId(user.getId()).contents("stampContents").build());
-        Stamp stamp2 = stampRepository.save(Stamp.builder().userId(user.getId()).contents("stampContents").build());
+        Stamp stamp1 = stampRepository.save(Stamp.builder().userId(newUser.getId()).contents("stampContents").build());
+        Stamp stamp2 = stampRepository.save(Stamp.builder().userId(newUser.getId()).contents("stampContents").build());
 
         //then
-        Assertions.assertEquals(List.of(stamp1, stamp2), stampRepository.findAllByUserId(user.getId()));
+        Assertions.assertEquals(List.of(stamp1, stamp2), stampRepository.findAllByUserId(newUser.getId()));
     }
 
     @Test
     @DisplayName("SUCCESS_유저 아이디와 미션 아이디를 이용하여 유저의 스탬프 조회")
     void SUCCESS_findByUserIdAndMissionIdSuccess() {
         //given
-        final User user = userRepository.save(new User());
         final Long missionId = 1L;
 
         //when
-        Stamp stamp = stampRepository.save(Stamp.builder().userId(user.getId()).missionId(missionId).contents("stampContents").build());
-        Optional<Stamp> result = stampRepository.findByUserIdAndMissionId(user.getId(), missionId);
+        Stamp stamp = stampRepository.save(Stamp.builder().userId(newUser.getId()).missionId(missionId).contents("stampContents").build());
+        Optional<Stamp> result = stampRepository.findByUserIdAndMissionId(newUser.getId(), missionId);
 
         //then
         Assertions.assertEquals(Optional.of(stamp), result);
@@ -59,15 +63,14 @@ class StampRepositoryTest {
     @DisplayName("SUCCESS_유저 아이디를 이용하여 유저의 스탬프 모두 삭제")
     void SUCCESS_deleteAllByUserIdSuccess() {
         //given
-        final User user = userRepository.save(new User());
-        stampRepository.save(Stamp.builder().userId(user.getId()).contents("stampContents").build());
-        stampRepository.save(Stamp.builder().userId(user.getId()).contents("stampContents").build());
+        stampRepository.save(Stamp.builder().userId(newUser.getId()).contents("stampContents").build());
+        stampRepository.save(Stamp.builder().userId(newUser.getId()).contents("stampContents").build());
 
         //when
-        stampRepository.deleteAllByUserId(user.getId());
+        stampRepository.deleteAllByUserId(newUser.getId());
 
         //then
-        Assertions.assertEquals(List.of(), stampRepository.findAllByUserId(user.getId()));
+        Assertions.assertEquals(List.of(), stampRepository.findAllByUserId(newUser.getId()));
     }
 
 }
