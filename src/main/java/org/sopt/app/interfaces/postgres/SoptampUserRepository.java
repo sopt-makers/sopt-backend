b1bca2e9dafdae6540dcc0cc7eacf91a45d3cf14
kseysh <kseysh123@naver.com>
2024-03-24 17:26:06 +0900

    refactor: 사용하지 않는 레포지토리 메서드 삭제 (#217)

--- src/main/java/org/sopt/app/interfaces/postgres/SoptampUserRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/SoptampUserRepository.java
@@ -1,6 +1,5 @@
 package org.sopt.app.interfaces.postgres;
 
-import java.util.List;
 import java.util.Optional;
 import org.sopt.app.domain.entity.SoptampUser;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,4 @@ public interface SoptampUserRepository extends JpaRepository<SoptampUser, Long> 
     Optional<SoptampUser> findByUserId(Long userId);
 
     Optional<SoptampUser> findUserByNickname(String nickname);
-
-    List<SoptampUser> findAllByUserIdIn(List<Long> userIds);
 }
