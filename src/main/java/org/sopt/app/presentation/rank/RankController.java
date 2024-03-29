9ac3f6176803607980c5811a9790872899366aa3
kseysh <kseysh123@naver.com>
2024-03-29 15:25:32 +0900

    feat: findCurrentRanksByPart controller 추가 (#228)

--- src/main/java/org/sopt/app/presentation/rank/RankController.java
+++ src/main/java/org/sopt/app/presentation/rank/RankController.java
@@ -6,16 +6,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.app.application.mission.MissionService;
 import org.sopt.app.application.soptamp.SoptampPointService;
 import org.sopt.app.application.soptamp.SoptampUserService;
-import org.sopt.app.domain.entity.User;
+import org.sopt.app.domain.enums.Part;
+import org.sopt.app.presentation.stamp.StampRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,11 +51,25 @@ public class RankController {
             @ApiResponse(responseCode = "500", description = "server error", content = @Content)
     })
     @GetMapping("/current")
-    public ResponseEntity<List<RankResponse.RankMain>> findCurrentRanks(
-        @AuthenticationPrincipal User user
+    public ResponseEntity<List<RankResponse.RankMain>> findCurrentRanks() {
+        val soptampPointList = soptampPointService.findCurrentPointList();
+        val result = soptampUserService.findCurrentRanks(soptampPointList);
+        val response = rankResponseMapper.of(result);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Operation(summary = "파트 별 현재 기수 랭킹 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "part not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "server error", content = @Content)
+    })
+    @GetMapping("/current")
+    public ResponseEntity<List<RankResponse.RankMain>> findCurrentRanksByPart(
+            @Valid @ModelAttribute Part part
     ) {
-        val soptamPointList = soptampPointService.findCurrentPointList();
-        val result = soptampUserService.findCurrentRanks(soptamPointList);
+        val soptampPointList = soptampPointService.findCurrentPointListByPart();
+        val result = soptampUserService.findCurrentRanks(soptampPointList);
         val response = rankResponseMapper.of(result);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
