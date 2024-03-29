d1631725486bdbbe88c9f6305d3d27bed630c070
kseysh <kseysh123@naver.com>
2024-03-29 18:14:16 +0900

    feat: Part enum값 수정 (#228)

--- src/main/java/org/sopt/app/domain/enums/Part.java
+++ src/main/java/org/sopt/app/domain/enums/Part.java
@@ -1,11 +1,18 @@
 package org.sopt.app.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Part {
-    ALL,
-    PLAN,
-    DESIGN,
-    ANDROID,
-    IOS,
-    WEB,
-    SERVER
+    PLAN("기획"),
+    DESIGN("디자인"),
+    ANDROID("안드"),
+    IOS("아요"),
+    WEB("웹"),
+    SERVER("서버"),
+    ;
+
+    private final String partName;
 }
