44d5d5fda359275dbbcc2d8905cebd0ce7ce3fc4
kseysh <kseysh123@naver.com>
2024-04-03 00:58:56 +0900

    fix: FriendService에서 데이터베이스에 저장되어 있는 값에 의존하지 않도록 수정 (#227)

--- src/test/java/org/sopt/app/application/FriendServiceTest.java
+++ src/test/java/org/sopt/app/application/FriendServiceTest.java
@@ -1,21 +1,37 @@
 package org.sopt.app.application;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.sopt.app.application.poke.FriendService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.sopt.app.interfaces.postgres.FriendRepository;
 
-@SpringBootTest
-public class FriendServiceTest {
+@ExtendWith(MockitoExtension.class)
+class FriendServiceTest {
 
-    @Autowired
+    @Mock
+    private FriendRepository friendRepository;
+
+    @InjectMocks
     private FriendService friendService;
 
     @Test
-    @DisplayName("200 - getIsNewUserSuccess")
+    @DisplayName("SUCCESS_친구가 없다면 새로운 유저로 판단하여 true 반환")
     void getIsNewUserSuccess() {
-        Assertions.assertEquals(true, friendService.getIsNewUser(1L));
+        //given
+        final Long anyUserId = anyLong();
+        //when
+        when(friendRepository.findAllByFriendUserId(anyUserId)).thenReturn(List.of());
+        when(friendRepository.findAllByUserIdAndFriendUserIdIn(anyUserId, List.of())).thenReturn(List.of());
+        //then
+        Assertions.assertTrue(friendService.getIsNewUser(anyUserId));
     }
 }
