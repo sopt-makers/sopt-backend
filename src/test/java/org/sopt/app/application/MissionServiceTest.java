3da12bfb0e75fd9affe0437823c40e19213eb2e6
kseysh <kseysh123@naver.com>
2024-04-16 19:28:06 +0900

    feat: getIncompleteMission 성공 케이스 추가 (#217)

--- src/test/java/org/sopt/app/application/MissionServiceTest.java
+++ src/test/java/org/sopt/app/application/MissionServiceTest.java
@@ -1,4 +1,4 @@
-package org.sopt.app.application.mission;
+package org.sopt.app.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,11 +9,11 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sopt.app.application.mission.MissionInfo.Completeness;
+import org.sopt.app.application.mission.MissionService;
 import org.sopt.app.domain.entity.Mission;
 import org.sopt.app.domain.entity.Stamp;
 import org.sopt.app.interfaces.postgres.MissionRepository;
@@ -32,24 +32,26 @@ class MissionServiceTest {
     @InjectMocks
     private MissionService missionService;
 
+    private List<Stamp> stampList = List.of(
+            Stamp.builder().missionId(1L).build(),
+            Stamp.builder().missionId(2L).build()
+    );
+
+    private List<Mission> displayedMissionList = List.of(
+            Mission.builder().id(1L).title("title1").level(1).display(true).build(),
+            Mission.builder().id(2L).title("title2").level(2).display(true).build(),
+            Mission.builder().id(3L).title("title3").level(3).display(true).build()
+    );
+
     @Test
     @DisplayName("SUCCESS_모든 미션 조회")
     void SUCCESS_findAllMission() {
         // given
         final Long anyUserId = anyLong();
-        final List<Stamp> stampList = List.of(
-                Stamp.builder().missionId(1L).build(),
-                Stamp.builder().missionId(2L).build()
-        );
-        final List<Mission> missionList = List.of(
-                Mission.builder().id(1L).title("title1").level(1).build(),
-                Mission.builder().id(2L).title("title2").level(2).build(),
-                Mission.builder().id(3L).title("title3").level(3).build()
-        );
 
         // when
         when(stampRepository.findAllByUserId(anyUserId)).thenReturn(stampList);
-        when(missionRepository.findAllByDisplay(true)).thenReturn(missionList);
+        when(missionRepository.findAllByDisplay(true)).thenReturn(displayedMissionList);
 
         List<Completeness> result = missionService.findAllMission(anyUserId);
         List<Completeness> expected = List.of(
@@ -91,12 +93,7 @@ class MissionServiceTest {
     void SUCCESS_getCompleteMission() {
         // given
         final Long anyUserId = anyLong();
-        final List<Stamp> stampList = List.of(
-                Stamp.builder().missionId(1L).build(),
-                Stamp.builder().missionId(2L).build()
-        );
         final List<Long> missionIdList = List.of(1L, 2L);
-
 
         // when
         final List<Mission> expected = List.of(
@@ -112,11 +109,26 @@ class MissionServiceTest {
         assertThat(result).usingRecursiveComparison().isEqualTo(expected);
     }
 
-    /* TODO: implement following test case
     @Test
-    void getIncompleteMission() {
+    @DisplayName("SUCCESS_미완료한 미션 조회")
+    void SUCCESS_getIncompleteMission() {
+        // given
+        Long anyUserId = anyLong();
+
+        // when
+        List<Mission> expected = List.of(Mission.builder().id(3L).build());
+
+        when(stampRepository.findAllByUserId(anyUserId)).thenReturn(stampList);
+        when(missionRepository.findMissionInOrderByLevelAndTitleAndDisplayTrue(any())).thenReturn(expected);
+        when(missionRepository.findAllByDisplay(true)).thenReturn(displayedMissionList);
+
+        List<Mission> result = missionService.getIncompleteMission(anyUserId);
+
+        // then
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
     }
 
+    /* TODO: implement following test case
     @Test
     void getMissionById() {
     }
