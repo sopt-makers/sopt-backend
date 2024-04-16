933df8d14bd4018414b42d40d58e04caeaeef615
kseysh <kseysh123@naver.com>
2024-04-16 17:37:29 +0900

    feat: SoptampPointRepositoryTest 틀 추가 (#217)

---
+++ src/test/java/org/sopt/app/interfaces/postgres/SoptampPointRepositoryTest.java
@@ -0,0 +1,36 @@
+package org.sopt.app.interfaces.postgres;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class SoptampPointRepositoryTest {
+
+    @BeforeEach
+    void beforeTest() {
+
+    }
+
+    /* TODO: Implement following test
+
+    @Autowired
+    private SoptampPointRepository soptampPointRepository;
+
+    @Test
+    @DisplayName("SUCCESS_기수별 솝탬프 포인트 찾기")
+    void SUCCESS_findAllByGeneration() {
+
+    }
+
+    @Test
+    void SUCCESS_findAllBySoptampUserIdAndGeneration() {
+    }
+
+    @Test
+    void SUCCESS_findAllBySoptampUserIdInAndGeneration() {
+    }
+    */
+}
