ba094235cfa2d50430fd9ad1e44f883f09ff6acd
kseysh <kseysh123@naver.com>
2024-04-18 17:13:13 +0900

    refactor: 사용하지 않는 import문 삭제 (#217)

--- src/test/java/org/sopt/app/interfaces/postgres/SoptampPointRepositoryTest.java
+++ src/test/java/org/sopt/app/interfaces/postgres/SoptampPointRepositoryTest.java
@@ -1,7 +1,6 @@
 package org.sopt.app.interfaces.postgres;
 
 import java.util.List;
-import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
