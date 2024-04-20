f4a84ca2d68abb4f3bd5b1ec9e748235f77dba44
kseysh <kseysh123@naver.com>
2024-04-20 20:08:53 +0900

    feat: getStampInfo 성공 케이스 추가 (#217)

--- src/test/java/org/sopt/app/facade/SoptampFacadeTest.java
+++ src/test/java/org/sopt/app/facade/SoptampFacadeTest.java
@@ -1,5 +1,12 @@
 package org.sopt.app.facade;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,7 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.sopt.app.application.mission.MissionService;
 import org.sopt.app.application.s3.S3Service;
 import org.sopt.app.application.soptamp.SoptampPointService;
+import org.sopt.app.application.soptamp.SoptampUserInfo;
 import org.sopt.app.application.soptamp.SoptampUserService;
+import org.sopt.app.application.stamp.StampInfo;
 import org.sopt.app.application.stamp.StampService;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,12 +37,27 @@ class SoptampFacadeTest {
     @InjectMocks
     private SoptampFacade soptampFacade;
 
-    /* TODO: 아래 메서드 구현
 
     @Test
-    void getStampInfo() {
+    @DisplayName("SUCCESS_스탬프 조회하기")
+    void SUCCESS_getStampInfo() {
+        // given
+        final Long missionId = 1L;
+        final Long userId = 1L;
+        final String nickname = "anyNickname";
+        final SoptampUserInfo.SoptampUser userInfo = SoptampUserInfo.SoptampUser.builder().userId(userId).nickname(nickname).build();
+        final StampInfo.Stamp stampInfo = StampInfo.Stamp.builder().missionId(missionId).build();
+        given(soptampUserService.findByNickname(nickname)).willReturn(userInfo);
+        given(stampService.findStamp(missionId, userId)).willReturn(stampInfo);
+
+        // when
+        StampInfo.Stamp result = soptampFacade.getStampInfo(missionId, nickname);
+
+        // then
+        assertThat(result).usingRecursiveComparison().isEqualTo(stampInfo);
     }
 
+    /* TODO: 아래 메서드 구현
     @Test
     void uploadStamp() {
     }
