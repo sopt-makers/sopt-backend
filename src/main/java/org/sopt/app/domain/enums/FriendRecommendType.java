4afc3738c94c6803966c3b51e3fc72c9295672c4
kseysh <kseysh123@naver.com>
2024-06-02 19:28:25 +0900

    feat: recommend type 및 그에 따른 타이틀 추가 (#283)

---
+++ src/main/java/org/sopt/app/domain/enums/FriendRecommendType.java
@@ -0,0 +1,15 @@
+package org.sopt.app.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum FriendRecommendType {
+    GENERATION("나와 같은 %d기를 하고 있어요"),
+    MBTI("나와 MBTI가 같은 사람이에요"),
+    SOJU_CAPACITY("나와 주량을 가지고 있어요")
+    ;
+
+    private final String recommendTitle;
+}
