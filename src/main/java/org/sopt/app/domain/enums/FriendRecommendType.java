789840a99c9b8eef24632b54042655949af0c9c1
kseysh <kseysh123@naver.com>
2024-06-03 10:16:42 +0900

    chore: 친구 추천 타입 title 변경 (#283)

--- src/main/java/org/sopt/app/domain/enums/FriendRecommendType.java
+++ src/main/java/org/sopt/app/domain/enums/FriendRecommendType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum FriendRecommendType {
-    GENERATION("나와 같은 %d기를 하고 있어요"),
+    GENERATION("나와 같은 기수예요"),
     MBTI("나와 MBTI가 같은 사람이에요"),
     SOJU_CAPACITY("나와 주량을 가지고 있어요")
     ;
