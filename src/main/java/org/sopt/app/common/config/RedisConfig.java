43af1b75e937e57ba33cc5931f8290a3bebb49a1
kseysh <kseysh123@naver.com>
2024-06-30 19:46:06 +0900

    feat: redis config 설정 (#324)

---
+++ src/main/java/org/sopt/app/common/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package org.sopt.app.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+
+}
