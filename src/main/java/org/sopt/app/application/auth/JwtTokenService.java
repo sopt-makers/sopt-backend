5d37eb2837d500e92dd2a74c85d620659a74258e
kseysh <kseysh123@naver.com>
2024-07-02 14:52:04 +0900

    refactor: 사용되지 않는 import 제거 (#324)

--- src/main/java/org/sopt/app/application/auth/JwtTokenService.java
+++ src/main/java/org/sopt/app/application/auth/JwtTokenService.java
@@ -3,7 +3,6 @@ package org.sopt.app.application.auth;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
