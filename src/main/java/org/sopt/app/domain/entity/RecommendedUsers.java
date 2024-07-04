5647d207a9d0cda41fa8ca12f8d720714adfff61
kseysh <kseysh123@naver.com>
2024-07-04 19:52:29 +0900

    feat: 친구 추천에 사용될 model 생성 (#324)

--- src/main/java/org/sopt/app/domain/entity/RecommendedUsers.java
+++ src/main/java/org/sopt/app/domain/entity/RecommendedUsers.java
@@ -1,19 +1,27 @@
 package org.sopt.app.domain.entity;
 
-import javax.persistence.Id;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.sopt.app.application.auth.PlaygroundAuthInfo.PlaygroundUserIds;
-import org.sopt.app.application.auth.PlaygroundAuthInfo.RecommendFriendRequest;
+import lombok.NoArgsConstructor;
+import org.sopt.app.application.auth.RecommendFriendRequest;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @RedisHash(value = "recommendedUsers", timeToLive = 60 * 60 * 24L)
 public class RecommendedUsers {
-    @Id
-    private RecommendFriendRequest request;
 
-    private PlaygroundUserIds playgroundUserIds;
+    @Id
+    private String condition;
+
+    private List<Long> userIds;
+
+    public RecommendedUsers(final RecommendFriendRequest request, final List<Long> userIds) {
+        this.condition = request.toString();
+        this.userIds = userIds;
+    }
 }
