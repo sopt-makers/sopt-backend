d595bbaae7f27102673d04da808961199e2601a2
kseysh <kseysh123@naver.com>
2024-07-04 19:52:42 +0900

    feat: 친구 추천에 사용될 model CrudRepository 생성 (#324)

--- src/main/java/org/sopt/app/interfaces/postgres/RecommendedUserIdsRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/RecommendedUserIdsRepository.java
@@ -1,9 +1,12 @@
 package org.sopt.app.interfaces.postgres;
 
-import org.sopt.app.application.auth.PlaygroundAuthInfo.RecommendFriendRequest;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import org.sopt.app.domain.entity.RecommendedUsers;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecommendedUserIdsRepository extends CrudRepository<RecommendedUsers, RecommendFriendRequest>{
+public interface RecommendedUserIdsRepository extends CrudRepository<RecommendedUsers, String> {
 
+    @NotNull
+    Optional<RecommendedUsers> findByCondition(@NotNull String condition);
 }
