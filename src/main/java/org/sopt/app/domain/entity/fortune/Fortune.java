fd028e84f8fe7042b9f9c38e577538818d914d6c
kseysh <kseysh123@naver.com>
2024-09-19 01:23:15 +0900

    [feat] Fortune Entity 추가 (#341)

---
+++ src/main/java/org/sopt/app/domain/entity/fortune/Fortune.java
@@ -0,0 +1,25 @@
+package org.sopt.app.domain.entity.fortune;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Fortune {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private Long fortuneCardId;
+
+}
