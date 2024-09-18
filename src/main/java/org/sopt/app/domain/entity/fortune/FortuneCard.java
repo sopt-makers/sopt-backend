81092b7abf1ee832047e3ffb59409aea9c16d7b4
kseysh <kseysh123@naver.com>
2024-09-19 01:49:30 +0900

    [feat] FortuneCard Entity 변경 (#341)

--- src/main/java/org/sopt/app/domain/entity/fortune/FortuneCard.java
+++ src/main/java/org/sopt/app/domain/entity/fortune/FortuneCard.java
@@ -1,9 +1,6 @@
 package org.sopt.app.domain.entity.fortune;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import lombok.*;
 
 @Getter
@@ -17,6 +14,8 @@ public class FortuneCard {
 
     private String name;
 
-    private String title;
+    private String description;
+
+    private String imageUrl;
 
 }
