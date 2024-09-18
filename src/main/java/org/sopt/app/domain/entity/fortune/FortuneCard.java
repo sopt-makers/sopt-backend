b1f80afa2bfc30312b925a5ab5d2014ab6a6f593
kseysh <kseysh123@naver.com>
2024-09-19 01:23:21 +0900

    [feat] FortuneCard Entity 추가 (#341)

---
+++ src/main/java/org/sopt/app/domain/entity/fortune/FortuneCard.java
@@ -0,0 +1,22 @@
+package org.sopt.app.domain.entity.fortune;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FortuneCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String title;
+
+}
