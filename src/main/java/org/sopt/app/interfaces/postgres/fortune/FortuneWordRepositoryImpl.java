c43e125ea4dde109253c3aee91ced85efeee6c63
kseysh <kseysh123@naver.com>
2024-09-19 16:33:59 +0900

    [chore] Fortune -> FortuneWord로 네이밍 변경 (#341)

--- src/main/java/org/sopt/app/interfaces/postgres/fortune/FortuneWordRepositoryImpl.java
+++ src/main/java/org/sopt/app/interfaces/postgres/fortune/FortuneWordRepositoryImpl.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.sopt.app.domain.entity.fortune.FortuneWord;
-import org.sopt.app.domain.entity.fortune.QFortune;
+import org.sopt.app.domain.entity.fortune.QFortuneWord;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,12 +16,12 @@ public class FortuneWordRepositoryImpl implements FortuneWordRepository {
 
     @Override
     public Optional<FortuneWord> findByRelatedUserIdAndCheckedAt(final Long userId, final LocalDate checkedAt) {
-        QFortune fortune = new QFortune("fortune");
+        QFortuneWord fortuneWord = new QFortuneWord("fortune");
 
         return Optional.ofNullable(
-                queryFactory.select(fortune)
-                        .from(fortune)
-                        .where(fortune.id.eq(1L))
+                queryFactory.select(fortuneWord)
+                        .from(fortuneWord)
+                        .where(fortuneWord.id.eq(1L))
                         .fetchOne()
         );
     }
