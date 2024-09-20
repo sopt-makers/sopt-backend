34e3861ae5f0ea13c2887bdfdb25cff930ba9ba9
kseysh <kseysh123@naver.com>
2024-09-20 19:02:53 +0900

    [refactor] 친구의 playgroundId를 반환하는 객체 추가 (#341)

---
+++ src/main/java/org/sopt/app/application/friend/FriendProvider.java
@@ -0,0 +1,17 @@
+package org.sopt.app.application.friend;
+
+import java.util.Set;
+import org.sopt.app.application.auth.dto.PlaygroundProfileInfo.OwnPlaygroundProfile;
+import org.sopt.app.application.friend.friend_recommender.FriendRecommender;
+import org.sopt.app.application.friend.friend_recommender.FriendRecommenderFactory;
+import org.sopt.app.domain.enums.FriendRecommendType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FriendProvider {
+    public Set<Long> getFriendPlaygroundIdsByType(OwnPlaygroundProfile profile, FriendRecommendType type) {
+        FriendRecommenderFactory factory = new FriendRecommenderFactory();
+        FriendRecommender recommender = factory.create(type);
+        return recommender.recommend(profile);
+    }
+}
