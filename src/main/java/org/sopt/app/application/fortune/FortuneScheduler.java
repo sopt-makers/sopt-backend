92e01f0a39d5898d63513a4e8505cafb2e81d782
rlarlgnszx <rlarlgnszx@naver.com>
2024-09-21 17:36:51 +0900

    [chore] 주석제거 및 변수 설정 (#344)

--- src/main/java/org/sopt/app/application/fortune/FortuneScheduler.java
+++ src/main/java/org/sopt/app/application/fortune/FortuneScheduler.java
@@ -1,11 +1,14 @@
 package org.sopt.app.application.fortune;
 
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.sopt.app.application.notification.NotificationService;
+import org.sopt.app.common.event.Events;
 import org.sopt.app.domain.enums.NotificationCategory;
 import org.sopt.app.domain.enums.NotificationType;
-import org.sopt.app.presentation.notification.NotificationRequest;
+import org.sopt.app.presentation.notification.NotificationRequest.RegisterNotificationRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,22 +16,27 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class FortuneScheduler {
     private final NotificationService notificationService;
+    @Value("${app.base.url}")
+    private String baseUrl;
+
     // 매일 아침 9시에 실행되는 스케줄러
     @Scheduled(cron = "0 0 9 * * ?")
     public void runDailyFortuneCreation() {
-        NotificationRequest.RegisterNotificationRequest registerNotificationRequest = createFortuneNotificationRequest();
-        notificationService.registerNotification(registerNotificationRequest);
+        RegisterNotificationRequest registerNotificationRequest = createFortuneNotificationRequest();
+        List<Long> playgroundIds = notificationService.registerNotification(registerNotificationRequest);
+        playgroundIds
+                .forEach(userId -> Events.raise(FortuneEvent.of(userId)));
     }
 
-    private NotificationRequest.RegisterNotificationRequest createFortuneNotificationRequest() {
-        return NotificationRequest.RegisterNotificationRequest.builder()
-                .notificationId(UUID.randomUUID().toString())  // 고유한 알림 ID
-                .title("오늘의 솝마디")  // 알림 제목
-                .content("오늘의 솝마디를 확인해보세요!")  // 알림 내용
-                .type(NotificationType.SEND_ALL)  // 모든 사용자에게 보낼 경우
-                .category(NotificationCategory.NEWS)  // 알림 카테고리
-                .deepLink("https://app.dev.sopt.org/api/v2/fortune/word")  // Fortune API URL
-                .webLink("https://app.dev.sopt.org/api/v2/fortune/word")  // 웹 링크
+    private RegisterNotificationRequest createFortuneNotificationRequest() {
+        return RegisterNotificationRequest.builder()
+                .notificationId(UUID.randomUUID().toString())
+                .title("오늘의 솝마디")
+                .content("오늘의 솝마디를 확인해보세요!")
+                .type(NotificationType.SEND_ALL)
+                .category(NotificationCategory.NEWS)
+                .deepLink(baseUrl + "/api/v2/fortune/word")
+                .webLink(baseUrl + "/api/v2/fortune/word")
                 .build();
     }
 }
