2843fe1006d5f5c24abd8418e64b5e8194118596
rlarlgnszx <rlarlgnszx@naver.com>
2024-09-25 01:23:02 +0900

    [chore] 와일드카드 사용 (#347)

--- src/main/java/org/sopt/app/domain/entity/soptamp/Mission.java
+++ src/main/java/org/sopt/app/domain/entity/soptamp/Mission.java
@@ -2,17 +2,9 @@ package org.sopt.app.domain.entity.soptamp;
 
 
 import io.hypersistence.utils.hibernate.type.array.StringArrayType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Type;
 
 @Entity
