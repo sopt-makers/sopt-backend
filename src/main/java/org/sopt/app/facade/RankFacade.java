95f4b9e696d32486aad0e79610e206b0bee7b8f5
kseysh <kseysh123@naver.com>
2024-09-29 18:07:16 +0900

    [refactor] 불필요한 공백 삭제 (#333)

--- src/main/java/org/sopt/app/facade/RankFacade.java
+++ src/main/java/org/sopt/app/facade/RankFacade.java
@@ -5,12 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.app.application.rank.SoptampPartRankCalculator;
 import org.sopt.app.application.rank.SoptampUserRankCalculator;
 import org.sopt.app.application.soptamp.SoptampPointInfo.Main;
-import org.sopt.app.application.soptamp.SoptampPointInfo.PartPoint;
 import org.sopt.app.application.soptamp.SoptampPointInfo.PartRank;
 import org.sopt.app.application.soptamp.SoptampUserFinder;
 import org.sopt.app.application.soptamp.SoptampUserInfo;
-import org.sopt.app.application.soptamp.SoptampUserService;
-import org.sopt.app.domain.entity.soptamp.SoptampUser;
 import org.sopt.app.domain.enums.Part;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,14 +18,12 @@ public class RankFacade {
 
     private final SoptampUserFinder soptampUserFinder;
 
-
     @Transactional(readOnly = true)
     public List<Main> findCurrentRanks() {
         List<SoptampUserInfo> soptampUserInfos = soptampUserFinder.findAllCurrentGenerationSoptampUsers();
         SoptampUserRankCalculator soptampUserRankCalculator = new SoptampUserRankCalculator(soptampUserInfos);
         return soptampUserRankCalculator.calculateRank();
     }
-
 
     @Transactional(readOnly = true)
     public List<Main> findCurrentRanksByPart(Part part) {
