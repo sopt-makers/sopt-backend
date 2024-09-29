0ce5e28b4cfd9ab5ff9e9800a35879e7f411946f
kseysh <kseysh123@naver.com>
2024-09-29 14:00:15 +0900

    [refactor] import문 단순화 (#333)

--- src/main/java/org/sopt/app/application/soptamp/SoptampUserInfo.java
+++ src/main/java/org/sopt/app/application/soptamp/SoptampUserInfo.java
@@ -1,9 +1,6 @@
 package org.sopt.app.application.soptamp;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import org.sopt.app.domain.entity.soptamp.SoptampUser;
 
 @Getter
