1bfff472e225ab3c09c9f102e10d7e48c5d46e3a
kseysh <kseysh123@naver.com>
2024-10-08 00:17:01 +0900

    [refactor] of 메서드로 UserProfile만드는 로직 단순화 (#399)

--- src/main/java/org/sopt/app/application/user/UserProfile.java
+++ src/main/java/org/sopt/app/application/user/UserProfile.java
@@ -1,6 +1,7 @@
 package org.sopt.app.application.user;
 
 import lombok.*;
+import org.sopt.app.domain.entity.User;
 
 @Getter
 @Builder
@@ -9,5 +10,13 @@ public class UserProfile {
     private Long userId;
     private String name;
     private Long playgroundId;
+
+    public static UserProfile of(User user){
+        return UserProfile.builder()
+                .userId(user.getId())
+                .name(user.getUsername())
+                .playgroundId(user.getPlaygroundId())
+                .build();
+    }
 
 }
