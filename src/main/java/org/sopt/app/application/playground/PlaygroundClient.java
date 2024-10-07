5464f305d442e1773b1f13657eefaef2eab648ad
kseysh <kseysh123@naver.com>
2024-10-07 23:25:32 +0900

    [refactor] static import로 가독성 향 (#399)

--- src/main/java/org/sopt/app/application/playground/PlaygroundClient.java
+++ src/main/java/org/sopt/app/application/playground/PlaygroundClient.java
@@ -1,9 +1,7 @@
 package org.sopt.app.application.playground;
 
 
-import feign.HeaderMap;
-import feign.Param;
-import feign.RequestLine;
+import feign.*;
 import java.util.List;
 import java.util.Map;
 import org.sopt.app.application.auth.dto.PlaygroundAuthTokenInfo.RefreshedToken;
@@ -11,7 +9,7 @@ import org.sopt.app.application.playground.dto.PlaygroundPostInfo.PlaygroundPost
 import org.sopt.app.application.playground.dto.PlaygroundProfileInfo.*;
 import org.sopt.app.application.playground.dto.PlaygroundUserFindCondition;
 import org.sopt.app.application.playground.dto.RecommendedFriendInfo.PlaygroundUserIds;
-import org.sopt.app.presentation.auth.AppAuthRequest;
+import org.sopt.app.presentation.auth.AppAuthRequest.*;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,12 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface PlaygroundClient {
 
     @RequestLine("POST /api/v1/idp/sso/auth")
-    AppAuthRequest.AccessTokenRequest getAccessToken(@HeaderMap final Map<String, String> headers,
-            final AppAuthRequest.CodeRequest codeRequest);
+    AccessTokenRequest getAccessToken(@HeaderMap final Map<String, String> headers, final CodeRequest codeRequest);
 
     @RequestLine("POST /internal/api/v1/idp/auth/token")
-    RefreshedToken refreshPlaygroundToken(@HeaderMap Map<String, String> headers,
-            AppAuthRequest.AccessTokenRequest tokenRequest);
+    RefreshedToken refreshPlaygroundToken(@HeaderMap Map<String, String> headers, AccessTokenRequest tokenRequest);
 
     @RequestLine("GET /internal/api/v1/members/latest?generation={generation}")
     ActiveUserIds getPlaygroundUserIds(@HeaderMap Map<String, String> headers, @Param("generation") Long generation);
