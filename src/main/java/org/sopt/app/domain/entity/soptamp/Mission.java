62fe08d85e7eb4f0d0a20299efea151dcc021558
kseysh <kseysh123@naver.com>
2024-10-09 02:45:40 +0900

    [refactor] Mission에서 NotNull 제약조건 추가 및 default value 추가 (#401)

--- src/main/java/org/sopt/app/domain/entity/soptamp/Mission.java
+++ src/main/java/org/sopt/app/domain/entity/soptamp/Mission.java
@@ -2,8 +2,10 @@ package org.sopt.app.domain.entity.soptamp;
 
 import io.hypersistence.utils.hibernate.type.array.ListArrayType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Type;
 
 @Entity
@@ -18,10 +20,14 @@ public class Mission {
     @Column(name = "mission_id")
     private Long id;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private Integer level;
 
+    @NotNull
+    @ColumnDefault("false")
     private boolean display;
 
     @Type(value= ListArrayType.class)
