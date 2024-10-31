76f66c09c33646b567a1b9c6fe28c79f8a79166c
kseysh <kseysh123@naver.com>
2024-10-31 10:12:23 +0900

    [refactor] 유저 탈퇴시 관련한 오늘의 운세 데이터 삭제 (#336)

--- src/main/java/org/sopt/app/interfaces/postgres/fortune/UserFortuneRepository.java
+++ src/main/java/org/sopt/app/interfaces/postgres/fortune/UserFortuneRepository.java
@@ -3,7 +3,12 @@ package org.sopt.app.interfaces.postgres.fortune;
 import java.util.Optional;
 import org.sopt.app.domain.entity.fortune.UserFortune;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserFortuneRepository extends JpaRepository<UserFortune, Long> {
     Optional<UserFortune> findByUserId(final Long userId);
+
+    @Query("DELETE FROM UserFortune u WHERE u.userId = :userId")
+    void deleteAllByUserIdInQuery(@Param("userId") Long userId);
 }
