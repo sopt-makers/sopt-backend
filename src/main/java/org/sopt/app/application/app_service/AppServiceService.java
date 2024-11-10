e465f4bf0c411d615e669e6fa08e917f7af30e37
kseysh <kseysh123@naver.com>
2024-11-10 20:59:52 +0900

    [refactor] 불필요한 변수 삭제 (#433)

--- src/main/java/org/sopt/app/application/app_service/AppServiceService.java
+++ src/main/java/org/sopt/app/application/app_service/AppServiceService.java
@@ -2,9 +2,7 @@ package org.sopt.app.application.app_service;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import org.sopt.app.domain.entity.AppService;
 import org.sopt.app.interfaces.postgres.AppServiceRepository;
 import org.springframework.stereotype.Service;
@@ -16,14 +14,13 @@ public class AppServiceService {
     private final AppServiceRepository appServiceRepository;
 
     public List<AppServiceInfo.AppService> getAllAppService() {
-        val appServiceList = appServiceRepository.findAll();
-        return appServiceList.stream()
+        return appServiceRepository.findAll().stream()
                 .sorted(Comparator.comparing(AppService::getCreatedAt).reversed())
                 .map(appService -> AppServiceInfo.AppService.builder()
                         .serviceName(appService.getServiceName())
                         .activeUser(appService.getActiveUser())
                         .inactiveUser(appService.getInactiveUser())
                         .build())
-                .collect(Collectors.toList());
+                .toList();
     }
 }
