85107411c07facdd286e306b4cbc659d4c637e99
kseysh <kseysh123@naver.com>
2024-11-10 22:12:52 +0900

    [feat] 일정 전체보기 Controller 추가 (#428)

---
+++ src/main/java/org/sopt/app/application/calendar/CalendarService.java
@@ -0,0 +1,7 @@
+package org.sopt.app.application.calendar;
+
+import org.sopt.app.presentation.calendar.AllCalendarResponse;
+
+public interface CalendarService {
+    AllCalendarResponse getAllCalendar();
+}
---
+++ src/main/java/org/sopt/app/presentation/calendar/CalendarController.java
@@ -0,0 +1,30 @@
+package org.sopt.app.presentation.calendar;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.sopt.app.application.calendar.CalendarService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/calendar")
+public class CalendarController {
+
+    private final CalendarService calendarService;
+
+    @Operation(summary = "일정 전체 보기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "500", description = "server error", content = @Content)
+    })
+    @GetMapping("/all")
+    public ResponseEntity<AllCalendarResponse> getAllCalendar() {
+        return ResponseEntity.ok(
+            calendarService.getAllCalendar()
+        );
+    }
+}
