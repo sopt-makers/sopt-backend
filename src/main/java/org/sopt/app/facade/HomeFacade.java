3de6384c154342a49cb22fe1e16217607bed6fa7
rlarlgnszx <rlarlgnszx@naver.com>
2024-11-19 01:39:38 +0900

    [refactor] 기능 메소드로 변경 (#434)

--- src/main/java/org/sopt/app/facade/HomeFacade.java
+++ src/main/java/org/sopt/app/facade/HomeFacade.java
@@ -75,7 +75,7 @@ public class HomeFacade {
     }
   
     public List<EmploymentPostResponse> getHomeEmploymentPost(User  user) {
-        return playgroundAuthService.getPlaygroundEmploymentPost(user.getPlaygroundToken());
+        return playgroundAuthService.getPlaygroundEmploymentPostWithMemberInfo(user.getPlaygroundToken());
     }
 
 }
