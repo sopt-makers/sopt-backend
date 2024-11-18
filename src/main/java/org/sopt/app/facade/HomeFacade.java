28fff42e7ed9c04b64f71576c76f47bd84281ca0
rlarlgnszx <rlarlgnszx@naver.com>
2024-11-19 01:05:32 +0900

    [del] 불필요한 부분 제거 (#434)

--- src/main/java/org/sopt/app/facade/HomeFacade.java
+++ src/main/java/org/sopt/app/facade/HomeFacade.java
@@ -71,14 +71,7 @@ public class HomeFacade {
     }
 
     public List<RecentPostsResponse> getRecentPosts(User user) {
-        return playgroundAuthService.getRecentPosts(user.getPlaygroundToken()).stream()
-                .map(post -> RecentPostsResponse.builder()
-                        .id(post.getId())
-                        .title(post.getTitle())
-                        .category(post.getCategory())
-                        .isHotPost(post.isHotPost())
-                        .build()
-                ).toList();
+        return playgroundAuthService.getRecentPostsWithMemberInfo(user.getPlaygroundToken());
     }
   
     public List<EmploymentPostResponse> getHomeEmploymentPost(User  user) {
