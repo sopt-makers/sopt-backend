1a53d39689adfce221e62a36557d2e35a772d288
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-07-27 14:07:09 +0900

    [#564] feat(PlaygroundPopularPost): dto에 userId 추가

--- src/main/java/org/sopt/app/application/playground/dto/PlaygroundPopularPost.java
+++ src/main/java/org/sopt/app/application/playground/dto/PlaygroundPopularPost.java
@@ -1,9 +1,12 @@
 package org.sopt.app.application.playground.dto;
 
+import javax.annotation.Nullable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record PlaygroundPopularPost(
 	@JsonProperty("id") Long playgroundPostId,
+	@Nullable Long userId,
 	String profileImage,
 	String name,
 	String generationAndPart,
@@ -14,10 +17,10 @@ public record PlaygroundPopularPost(
 	String webLink
 ) {
 	public static PlaygroundPopularPost from(
-		Long postId, String profileImage, String name, String generationAndPart, int rank, String category, String content, String title, String webLink
+		Long postId, Long userId, String profileImage, String name, String generationAndPart, int rank, String category, String content, String title, String webLink
 	) {
 		return new PlaygroundPopularPost(
-			postId, profileImage, name, generationAndPart, rank, category, title, content, webLink
+			postId, userId, profileImage, name, generationAndPart, rank, category, title, content, webLink
 		);
 	}
 }
