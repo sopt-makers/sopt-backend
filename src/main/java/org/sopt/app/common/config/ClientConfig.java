85a54d286d3051729dcdf752a526b0779e015b52
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-11 17:20:00 +0900

    [#590] fix(ClientConfig): Feign401ErrorDecoder 빈 등록 및 모든 클라이언트에 적용

--- src/main/java/org/sopt/app/common/config/ClientConfig.java
+++ src/main/java/org/sopt/app/common/config/ClientConfig.java
@@ -5,10 +5,12 @@ import feign.codec.*;
 import feign.jackson.*;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
+
 import org.sopt.app.application.meeting.CrewClient;
 import org.sopt.app.application.platform.PlatformClient;
 import org.springframework.context.annotation.*;
 import org.sopt.app.application.playground.PlaygroundClient;
+import org.sopt.app.common.external.feign.Feign401ErrorDecoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -16,63 +18,73 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class ClientConfig {
 
-    @Value("${makers.playground.server}")
-    private String playgroundEndPoint;
+	@Value("${makers.playground.server}")
+	private String playgroundEndPoint;
 
-    @Value("${makers.crew.server}")
-    private String crewEndPoint;
+	@Value("${makers.crew.server}")
+	private String crewEndPoint;
 
-    @Value("${external.auth.url}")
-    private String platformEndPoint;
+	@Value("${external.auth.url}")
+	private String platformEndPoint;
 
-    @Bean
-    public PlaygroundClient playgroundClient() {
-        return Feign.builder()
-                .client(okHttpClient())
-                .encoder(encoder())
-                .decoder(decoder())
-                .logger(new Slf4jLogger(PlaygroundClient.class))
-                .logLevel(feignLoggerLevel())
-                .target(PlaygroundClient.class, playgroundEndPoint);
-    }
+	@Bean
+	public PlaygroundClient playgroundClient() {
+		return Feign.builder()
+			.client(okHttpClient())
+			.encoder(encoder())
+			.decoder(decoder())
+			.errorDecoder(errorDecoder())
+			.logger(new Slf4jLogger(PlaygroundClient.class))
+			.logLevel(feignLoggerLevel())
+			.target(PlaygroundClient.class, playgroundEndPoint);
+	}
 
-    @Bean
-    public CrewClient crewClient() {
-        return Feign.builder()
-                .client(okHttpClient())
-                .encoder(encoder())
-                .decoder(decoder())
-                .logger(new Slf4jLogger(CrewClient.class))
-                .logLevel(feignLoggerLevel())
-                .target(CrewClient.class, crewEndPoint);
-    }
+	@Bean
+	public CrewClient crewClient() {
+		return Feign.builder()
+			.client(okHttpClient())
+			.encoder(encoder())
+			.decoder(decoder())
+			.errorDecoder(errorDecoder())
+			.logger(new Slf4jLogger(CrewClient.class))
+			.logLevel(feignLoggerLevel())
+			.target(CrewClient.class, crewEndPoint);
+	}
 
-    @Bean
-    public PlatformClient platformClient() {
-        return Feign.builder()
-                .client(okHttpClient())
-                .encoder(encoder())
-                .decoder(decoder())
-                .logger(new Slf4jLogger(CrewClient.class))
-                .logLevel(feignLoggerLevel())
-                .target(PlatformClient.class, platformEndPoint);
-    }
+	@Bean
+	public PlatformClient platformClient() {
+		return Feign.builder()
+			.client(okHttpClient())
+			.encoder(encoder())
+			.decoder(decoder())
+			.errorDecoder(errorDecoder())
+			.logger(new Slf4jLogger(PlatformClient.class))
+			.logLevel(feignLoggerLevel())
+			.target(PlatformClient.class, platformEndPoint);
+	}
 
-    @Bean
-    public Encoder encoder() {
-        return new JacksonEncoder();
-    }
-    @Bean
-    public Decoder decoder() {
-        return new JacksonDecoder();
-    }
-    @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.HEADERS;
-    }
-    @Bean
-    public OkHttpClient okHttpClient() {
-        return new OkHttpClient();
-    }
+	@Bean
+	public Encoder encoder() {
+		return new JacksonEncoder();
+	}
 
+	@Bean
+	public Decoder decoder() {
+		return new JacksonDecoder();
+	}
+
+	@Bean
+	public ErrorDecoder errorDecoder() {
+		return new Feign401ErrorDecoder();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.HEADERS;
+	}
+
+	@Bean
+	public OkHttpClient okHttpClient() {
+		return new OkHttpClient();
+	}
 }
