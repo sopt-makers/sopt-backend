1e263b9b5334f3f741581d58ec605b601312b19a
jher235 <tim668666@gmail.com>
2025-09-22 22:08:10 +0900

    [#609] feat: UserFixture 중앙인증화로 수정된 유저에 맞게 수정

--- src/test/java/org/sopt/app/common/fixtures/UserFixture.java
+++ src/test/java/org/sopt/app/common/fixtures/UserFixture.java
@@ -1,18 +1,14 @@
-// package org.sopt.app.common.fixtures;
-//
-// import org.sopt.app.domain.entity.User;
-//
-// public class UserFixture {
-//
-//     public static Long myAppUserId = 100L;
-//     public static Long myPlaygroundId = 1000L;
-//     public static String dummyPlaygroundToken = "token";
-//
-//     public static User createMyAppUser() {
-//         return User.builder()
-//                 .id(myAppUserId)
-//                 .playgroundId(myPlaygroundId)
-//                 .playgroundToken(dummyPlaygroundToken)
-//                 .build();
-//     }
-// }
+ package org.sopt.app.common.fixtures;
+
+ import org.sopt.app.domain.entity.User;
+
+ public class UserFixture {
+
+     public static Long myAppUserId = 100L;
+
+     public static User createMyAppUser() {
+         return User.builder()
+                 .id(myAppUserId)
+                 .build();
+     }
+ }
