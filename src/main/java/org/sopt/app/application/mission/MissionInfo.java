99629a38e2a318d79a47879fb0c3048d47212653
jher235 <tim668666@gmail.com>
2025-12-10 16:22:55 +0900

    [#659] feat: 팀미션 Dto 추가

--- src/main/java/org/sopt/app/application/mission/MissionInfo.java
+++ src/main/java/org/sopt/app/application/mission/MissionInfo.java
@@ -1,11 +1,13 @@
 package org.sopt.app.application.mission;
 
 import java.util.List;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.sopt.app.domain.entity.soptamp.Mission;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MissionInfo {
@@ -30,6 +32,34 @@ public class MissionInfo {
 
         public static Level of(Integer level) {
             return new Level(level);
+        }
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class TeamMissionInfo {
+
+        private Long id;
+        private String title;
+        private String ownerName;
+        private Integer level;
+        private List<String> profileImage;
+        private boolean isCompleted;
+
+        public static TeamMissionInfo of(
+            Mission mission,
+            boolean isCompleted,
+            Optional<String> ownerName
+        ) {
+            return TeamMissionInfo.builder()
+                .id(mission.getId())
+                .title(mission.getTitle())
+                .ownerName(ownerName.orElse(null))
+                .level(mission.getLevel())
+                .profileImage(mission.getProfileImage())
+                .isCompleted(isCompleted)
+                .build();
         }
     }
 }
