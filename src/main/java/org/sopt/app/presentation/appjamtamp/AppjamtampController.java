314c09346d80010e800e712a26956d75bb432d16
jher235 <tim668666@gmail.com>
2025-12-11 14:26:10 +0900

    [#662] feat: 앱팀탬프 스탬프 상세 조회 presentation layer 구현

--- src/main/java/org/sopt/app/presentation/appjamtamp/AppjamtampController.java
+++ src/main/java/org/sopt/app/presentation/appjamtamp/AppjamtampController.java
@@ -5,16 +5,18 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.sopt.app.domain.enums.TeamNumber;
+import org.sopt.app.facade.AppjamtampFacade;
 import org.sopt.app.facade.MissionFacade;
-import org.sopt.app.facade.SoptampFacade;
 import org.sopt.app.presentation.appjamtamp.AppjamtampResponse.AppjamMissionResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppjamtampController {
 
     private final MissionFacade missionFacade;
-    private final SoptampFacade soptampFacade;
+    private final AppjamtampFacade appjamtampFacade;
     private final AppjamtampResponseMapper appjamtampResponseMapper;
 
     @Operation(summary = "앱잼탬프 미션 목록 조회하기")
@@ -35,7 +37,7 @@ public class AppjamtampController {
         @ApiResponse(responseCode = "500", description = "server error", content = @Content)
     })
     @GetMapping("/mission")
-    public ResponseEntity<TeamMissionResponses> getMissions(
+    public ResponseEntity<AppjamMissionResponses> getMissions(
         @RequestParam TeamNumber teamNumber,
         @RequestParam(required = false) Boolean isCompleted
     ) {
@@ -43,4 +45,21 @@ public class AppjamtampController {
         val response = appjamtampResponseMapper.of(result);
         return ResponseEntity.ok(response);
     }
+
+    @Operation(summary = "앱잼탬프 스탬프 조회하기")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "success"),
+        @ApiResponse(responseCode = "500", description = "server error", content = @Content)
+    })
+    @GetMapping("/stamp")
+    public ResponseEntity<AppjamtampResponse.AppjamtampView> getMissions(
+        @AuthenticationPrincipal Long userId,
+        @Valid @ModelAttribute AppjamtampRequest.FindStampRequest findStampRequest
+    ) {
+        val result = appjamtampFacade.getAppjamtamps(userId, findStampRequest.getMissionId(),
+            findStampRequest.getNickname());
+        val response = appjamtampResponseMapper.of(result);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/org/sopt/app/presentation/appjamtamp/AppjamtampResponseMapper.java
+++ src/main/java/org/sopt/app/presentation/appjamtamp/AppjamtampResponseMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.sopt.app.application.mission.MissionInfo.AppjamMissionInfo;
 import org.sopt.app.application.mission.MissionInfo.AppjamMissionInfos;
+import org.sopt.app.application.stamp.StampInfo.AppjamtampView;
 import org.sopt.app.presentation.appjamtamp.AppjamtampResponse.AppjamMissionResponse;
 import org.sopt.app.presentation.appjamtamp.AppjamtampResponse.AppjamMissionResponses;
 
@@ -21,4 +22,25 @@ public interface AppjamtampResponseMapper {
     // TeamMissionInfo to TeamMissionResponse
     @Mapping(source = "completed", target = "isCompleted")
     AppjamMissionResponse toResponse(AppjamMissionInfo info);
+
+    default AppjamtampResponse.AppjamtampView of(AppjamtampView appjamtampView) {
+        return AppjamtampResponse.AppjamtampView.builder()
+            .id(appjamtampView.getId())
+            .contents(appjamtampView.getContents())
+            .images(appjamtampView.getImages())
+            .activityDate(appjamtampView.getActivityDate())
+            .createdAt(appjamtampView.getCreatedAt())
+            .updatedAt(appjamtampView.getUpdatedAt())
+            .missionId(appjamtampView.getMissionId())
+            .teamNumber(appjamtampView.getTeamNumber())
+            .teamName(appjamtampView.getTeamName())
+            .ownerNickname(appjamtampView.getOwnerNickName())
+            .ownerProfileImage(appjamtampView.getOwnerProfileImage())
+            .clapCount(appjamtampView.getClapCount())
+            .viewCount(appjamtampView.getViewCount())
+            .myClapCount(appjamtampView.getMyClapCount())
+            .isMine(appjamtampView.isMine())
+            .build();
+    }
+
 }
