3d45399a23a8c9963819c59262b9f95d41e370b0
jher235 <tim668666@gmail.com>
2026-04-24 15:17:38 +0900

    [#720] feat: 배포 스크립트에 actuator 관련 포트 설정 추가

--- script/nginx_reload.sh
+++ script/nginx_reload.sh
@@ -2,10 +2,16 @@
 
 reload_nginx() {
     local PORT=$1
+    local ACTUATOR_PORT=$2
 
     echo "▶️ Nginx Reload (Port switching applied) ..."
 
+    # 메인 앱 포트 스위칭
     echo "set \$service_url http://127.0.0.1:${PORT};" | sudo tee /etc/nginx/conf.d/app-url.inc
+    # Actuator 모니터링 포트 스위칭
+    echo "set \$actuator_url http://127.0.0.1:${ACTUATOR_PORT};" | sudo tee /etc/nginx/conf.d/actuator-url.inc
+    
     sudo nginx -s reload
     echo "Current running Port after switching: $(sudo cat /etc/nginx/conf.d/app-url.inc)"
+    echo "Current running Actuator Port after switching: $(sudo cat /etc/nginx/conf.d/actuator-url.inc)"
 }
