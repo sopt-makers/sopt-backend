2354fcdcda1ca8f764f4342f1f4a4d87b6ba3033
jher235 <tim668666@gmail.com>
2026-05-28 03:36:34 +0900

    [#765] chore: 필드명 title로 변경

--- src/main/java/org/sopt/app/domain/entity/soptletter/SoptLetterTopic.java
+++ src/main/java/org/sopt/app/domain/entity/soptletter/SoptLetterTopic.java
@@ -1,5 +1,6 @@
 package org.sopt.app.domain.entity.soptletter;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class SoptLetterTopic extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String content;
+    @Column(nullable = false)
+    private String title;
 
 }
