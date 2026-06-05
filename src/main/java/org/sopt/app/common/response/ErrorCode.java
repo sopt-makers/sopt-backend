0c45d71c22ff872ed5640b12d157b8f73773ddf1
jher235 <tim668666@gmail.com>
2026-06-06 01:45:55 +0900

    [#775] feat: 일일 솝레터 작성 제한 초과 에러 코드 추가

--- src/main/java/org/sopt/app/common/response/ErrorCode.java
+++ src/main/java/org/sopt/app/common/response/ErrorCode.java
@@ -112,6 +112,7 @@ public enum ErrorCode {
     //SoptLetter
     SOPT_LETTER_NICKNAME_IS_FULL("사용 가능한 솝레터 닉네임이 없습니다.", HttpStatus.CONFLICT),
     SOPT_LETTER_PROFILE_NOT_FOUND("솝레터 프로필이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    SOPT_LETTER_DAILY_LIMIT_EXCEEDED("일일 솝레터 작성 제한을 초과했습니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String message;
