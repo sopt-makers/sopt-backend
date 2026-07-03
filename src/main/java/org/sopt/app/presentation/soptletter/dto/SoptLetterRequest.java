bc941b308b16b059ec068f57e32efbd04b18d1a7
kyoooooong <haileyre000b@naver.com>
2026-07-04 02:34:55 +0900

    [#809] feat: 솝레터 이모지 글자 수 검증 구현

--- src/main/java/org/sopt/app/presentation/soptletter/dto/SoptLetterRequest.java
+++ src/main/java/org/sopt/app/presentation/soptletter/dto/SoptLetterRequest.java
@@ -1,11 +1,11 @@
 package org.sopt.app.presentation.soptletter.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.sopt.app.common.validation.GraphemeSize;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SoptLetterRequest {
@@ -14,7 +14,7 @@ public class SoptLetterRequest {
     @ToString
     public static class WriteMessageRequest {
         @NotBlank(message = "메시지 내용은 필수입니다.")
-        @Size(min = 1, max = 350, message = "메시지는 공백을 포함하여 1자 이상 350자 이하로 작성해야 합니다.")
+        @GraphemeSize(max = 350, message = "메시지는 공백을 포함하여 1자 이상 350자 이하로 작성해야 합니다.")
         private String content;
     }
 
@@ -22,7 +22,7 @@ public class SoptLetterRequest {
     @ToString
     public static class UpdateMessageRequest {
         @NotBlank(message = "메시지 내용은 필수입니다.")
-        @Size(min = 1, max = 350, message = "메시지는 공백을 포함하여 1자 이상 350자 이하로 작성해야 합니다.")
+        @GraphemeSize(max = 350, message = "메시지는 공백을 포함하여 1자 이상 350자 이하로 작성해야 합니다.")
         private String content;
     }
 }
